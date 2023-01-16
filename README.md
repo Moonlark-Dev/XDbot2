63b86746dd33813ce64b9e7f3b6b459e7a38483c
This-is-XiaoDeng <1744793737@qq.com>
2023-01-16 16:32:49 +0800

    [add]README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# XDbot2
+<!--Logo位-->
+
+# IT Craft XDbot2
+
+简单、多功能QQBot，基于 Nonebot2+OnebotV11
+ 
+-----
+
+## 简介
+
+IT Craft XDBot2（下简称`XDbot2`）是一个简单的QQ功能型机器人，基于 Nonebot2 + OnebotV11 编写
+
+相比于[XDbot](https://github.com/This-is-XiaoDeng/XDbot)，XDbot2拥有这些特性
+
+- 更稳定：更强的错误处理和上报系统
+- 便于管理：强大的超管系统，轻松管理XDbot
+- 易于使用：更详细的指令文档和更易用的指令
+- 功能更丰富：在XDbot的基础上增加了更多功能
+
+## 使用
+
+您可以加入 XDbot用户群（159910125）以体验 XDbot2
+
+## 贡献
+
+您可以通过提交 [Issues](https://github.com/This-is-XiaoDeng/XDbot2/issues) 或 [Pull requests](https://github.com/This-is-XiaoDeng/XDbot2/pulls) 帮助改进这个项目
+
+# 鸣谢
+
+- [IT Craft Development Team](https://itcdt.top)
+
+<a href="https://pay.thisisxd.top/"><img src="https://img.shields.io/badge/Sponsor%20me!-green?logo=wechat&amp;logoColor=white&amp;style=flat" alt="Sponsor me!"></a>
+
--- src/plugins/Core/plugins/vote.py
+++ src/plugins/Core/plugins/vote.py
@@ -23,14 +23,27 @@ async def voteHandle(
     try:
         data = json.load(open("data/vote.data.json", encoding="utf-8"))
         argument = message.extract_plain_text().split("\n")
+        group = event.get_session_id().split("_")[1]
         argument[0] = argument[0].split(" ")
+        # 检查过期
+        nowTime = time.time()
+        length = 0
+        _data = data.copy()
+        for voteData in _data:
+            if nowTime >= voteData["endTime"]:
+                data[length]["status"] = "已结束"
+            length += 1
+        # 解析参数
         if argument[0][0] == "":
-            text = "XDbot2 群投票列表："
+            text = "XDbot2 群投票列表：\n"
             length = 0
             for item in data:
-                text += f"{length}. {item['title']}: {item['status']}"
+                if item["group"] == group:
+                    text += f"{length}. {item['title']}（{item['status']}）\n"
+                length += 1
             await vote.finish(text)
         elif argument[0][0] == "create" or argument[0][0] == "创建":
+            voteID = max(len(data) - 1, 0)
             if len(argument[0]) < 2:
                 argument[0].append(24)
             else:
@@ -44,30 +57,41 @@ async def voteHandle(
                 "sender": event.get_user_id(),
                 "startTime": time.time(),
                 "status": "进行中",
-                "group": event.get_session_id().split("_")[1],
+                "group": group,
                 "endTime": time.time() + 60 * 60 * argument[0][1]
             })
             json.dump(data, open("data/vote.data.json", "w", encoding="utf-8"))
-            await vote.finish(f"投票 #{len(data) - 2} 已创建")
+            await vote.finish(f"投票 #{voteID} 已创建")
         elif argument[0][0] == "view" or argument[0][0] == "查看":
             voteData = data[int(argument[0][1])]
             if event.get_session_id().split("_")[1] == voteData['group']:
-                text = f"（{voteData['status']}）{voteData['title']}\n \n"
+                text = f"{voteData['title']}（{voteData['status']}）\n \n"
                 length = 0
+                selected = 0
+                for c in voteData["choices"]:
+                    selected += len(c['user'])
                 for choice in voteData['choices']:
-                    text += f"{length}. {choice['name']}: {len(choice['user'])}\n"
+                    text += f"{length}. {choice['name']}（{len(choice['user'])}/{selected}）\n"
                     length += 1
                 text += f" \n发起人：{(await bot.get_stranger_info(user_id=voteData['sender']))['nickname']}({voteData['sender']})"
+                text += f"\n截止时间：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}"
                 await vote.finish(text)
             else:
                 await vote.finish("权限不足")
         elif argument[0][0] == "select" or argument[0][0] == "选择":
-            if event.get_session_id().split("_")[1] == data[int(argument[0][1])]['group']:
+            # 是否重复选择
+            for c in data[int(argument[0][1])]["choices"]:
+                if event.get_user_id() in c["user"]:
+                    await vote.finish("您已经参与过了")
+            # 选择
+            if event.get_session_id().split("_")[1] == data[int(argument[0][1])]['group'] and voteData["status"] != "已结束":
                 data[int(argument[0][1])]["choices"][int(
                     argument[0][2])]["user"].append(event.get_user_id())
                 json.dump(data, open(
                     "data/vote.data.json", "w", encoding="utf-8"))
                 await vote.finish("OK")
+            else:
+                await vote.finish("权限不足")
 
     except FinishedException:
         raise FinishedException()
