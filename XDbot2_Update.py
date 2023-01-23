3e85ed09ed0994b5c35e8819be12314fc9b9bfa9
This-is-XiaoDeng <1744793737@qq.com>
2023-01-23 16:44:37 +0800

    [update] 修改内部版本号（改了个寂寞）

--- XDbot2_Update.py
+++ XDbot2_Update.py
@@ -1,18 +1,3 @@
-# XDbot2 V2.0.0-beta master/af3df071
+# XDbot2 v2.0.103-beta
 
-import os
-import json
-
-os.chdir("./data")
-data = json.load(open("ban.banData.json", encoding="utf-8"))
-
-for key in list(data.keys()):
-    banList = []
-    for item in data[key]:
-        if type(item) == list:
-            banList.append(item[0])
-        else:
-            banList.append(item)
-    data[key] = banList
-json.dump(data, open("ban.banData.json", "w", encoding="utf-8"))
-print("已重写：data/ban.banData.json")
+print("欢迎使用：XDbot2 v2.0.103-beta")
