bdf35de59cecda63d1cd7f352cca326f9fb64384
This-is-XiaoDeng <1744793737@qq.com>
2023-01-24 10:45:42 +0800

    [fix] 在`linuxman`使用异步请求

--- src/plugins/Core/plugins/linuxman.py
+++ src/plugins/Core/plugins/linuxman.py
@@ -1,13 +1,12 @@
 from nonebot.adapters.onebot.v11.bot import Bot
-from nonebot.adapters.onebot.v11.event import GroupMessageEvent
+# from nonebot.adapters.onebot.v11.event import GroupMessageEvent
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.params import CommandArg
 from nonebot.exception import FinishedException
 from nonebot import on_command
 import json
 import traceback
-from urllib import request as req
-import urllib.error
+import httpx
 
 ctrlGroup = json.load(open("data/ctrl.json", encoding="utf-8"))["control"]
 linuxman = on_command("linuxman")
@@ -15,30 +14,15 @@ linuxman = on_command("linuxman")
 
 @linuxman.handle()
 async def linuxmanHandle(bot: Bot,
-                         event: GroupMessageEvent,
                          message: Message = CommandArg()):
     try:
-        argument = message.extract_plain_text().split(" ")
-        try:
-            with req.urlopen(
-                    f"https://man.archlinux.org/man/{argument[0]}.txt") as fp:
-                manpage = fp.read().decode("utf-8")
-                await bot.call_api(
-                    api="seng_group_forward_msg",
-                    messages=[{
-                        "type": "node",
-                        "data": {
-                            "name": "XDbot2 LINUXMAN",
-                            "uin": str((await
-                                        bot.get_login_info())['user_id']),
-                            "content": str(manpage)
-                        }
-                    }],
-                    group=str(event.group_id))
-                await linuxman.finish()
-        except urllib.error.HTTPError as e:
-            if e.status == 404:
-                await linuxman.finish("找不到manpage")
+        argument = message.extract_plain_text()
+        async with httpx.AsyncClient() as client:
+            req = await client.get(argument)
+            text = req.read().decode("utf-8")
+        if req.status_code == 404:
+            await linuxman.finish("找不到收册页")
+        await linuxman.finish(text)
     except FinishedException:
         raise FinishedException()
     except Exception:
