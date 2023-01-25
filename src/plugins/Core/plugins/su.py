fba2044bc95bdff08d43b97495ac2fd2e8bb5879
This-is-XiaoDeng <1744793737@qq.com>
2023-01-25 21:29:43 +0800

    [update] 整理代码

--- src/plugins/Core/plugins/su.py
+++ src/plugins/Core/plugins/su.py
@@ -161,17 +161,19 @@ async def suHandle(bot: Bot, message: Message = CommandArg()):
                 data['count'] += 1
                 json.dump(data,
                           open("data/cave.data.json", "w", encoding="utf-8"))
-            elif argument[1] in ["modify","修改"]:
+            elif argument[1] in ["modify", "修改"]:
                 data = json.load(open("data/cave.data.json", encoding="utf-8"))
                 if argument[3] == "sender":
-                    if argument[4] in ["name","nickname"]:
-                        data[argument[2]]["sender"] = {"type":"nickname","name":argument[5]}
-                    elif argument[4] in ["id","qq"]:
+                    if argument[4] in ["name", "nickname"]:
+                        data[argument[2]]["sender"] = {
+                            "type": "nickname", "name": argument[5]}
+                    elif argument[4] in ["id", "qq"]:
                         data[argument[2]]["sender"] = argument[5]
-                    elif argument[4] in ["unknown","unkown"]:
-                        data[argument[2]]["sender"] = {"type":"unknown"}
+                    elif argument[4] in ["unknown", "unkown"]:
+                        data[argument[2]]["sender"] = {"type": "unknown"}
                 elif argument[3] == "text":
-                    data[argument[2]]["text"] = argument[4].replace("%20"," ").replace(r"\n", "\n")
+                    data[argument[2]]["text"] = argument[4].replace(
+                        "%20", " ").replace(r"\n", "\n")
                 json.dump(data,
                           open("data/cave.data.json", "w", encoding="utf-8"))
         elif argument[0] == "give" or argument[0] == "给予":
@@ -255,8 +257,8 @@ async def suHandle(bot: Bot, message: Message = CommandArg()):
                     "price": int(argument[4]),
                     "info": None,
                     "data": json.loads(argument[5]),
-                    "nickname":s_nickname,
-                    "user_id":s_user_id
+                    "nickname": s_nickname,
+                    "user_id": s_user_id
                 })
                 try:
                     data[-1]["maxBuy"] = int(argument[6])
