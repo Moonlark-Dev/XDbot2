d3827b52c1596bf660b4e35af82abbd218a5cc57
xxtg666 <1747433912@qq.com>
2023-01-28 23:09:37 +0800

    [fix] 添加回声洞的反馈本地化错误

--- src/plugins/Core/plugins/cave.py
+++ src/plugins/Core/plugins/cave.py
@@ -113,7 +113,7 @@ async def cave_handle(bot: Bot, event: MessageEvent, message: Message = CommandA
             # 写入数据
             json.dump(data, open("data/cave.data.json", "w", encoding="utf-8"))
             await cave.finish(
-                _lang.text("cave.added", [{data["count"] - 1}], event.get_user_id())
+                _lang.text("cave.added", [data["count"] - 1], event.get_user_id())
             )
 
         elif argument[0] in ["-g", "查询"]:
