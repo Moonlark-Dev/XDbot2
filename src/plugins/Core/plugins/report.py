8340b3cb7d4cfeadce87edd4a3b986645f4285c4
This-is-XiaoDeng <1744793737@qq.com>
2023-02-17 23:53:16 +0800

    [update] `/report`时将消息转发到控制中心（Issue#38）

--- src/plugins/Core/plugins/report.py
+++ src/plugins/Core/plugins/report.py
@@ -1,12 +1,16 @@
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters.onebot.v11 import Message, MessageEvent
+from nonebot.params import CommandArg
 from . import _lang
+from . import _error
 
 report = on_command("report", aliases={"反馈"})
 
 
 @report.handle()
-async def handle(event: MessageEvent):
+async def handle(event: MessageEvent, message: Message = CommandArg()):
+    await _error.report(("「新反馈」\n"
+        f"{message}\n"
+        f"{event.get_session_id()}\n"))
     await report.finish(
-        _lang.text("report.github", [], event.get_user_id()), at_sender=True
-    )
+        _lang.text("report.github", [], event.get_user_id()), at_sender=True)
