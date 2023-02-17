0dc6feaa7284598e504e20516a3fe9c028ddff1c
This-is-XiaoDeng <1744793737@qq.com>
2023-02-17 18:12:24 +0800

    [update] 基础规则操作

---
+++ src/plugins/Core/plugins/rule_compiler/rule/compiler.py
@@ -0,0 +1,15 @@
+import lexer
+import sys
+import parser
+import visitor
+
+
+def build(path: str, to_path: str = "./") -> None:
+    with open(f"{path}.xr", "r", encoding="utf-8") as f:
+        file = f.read()
+    p = parser.Parser(lexer.parse(file))
+    visitor.codegen(p.parse(), f"{to_path}{path}")
+
+
+if __name__ == "__main__":
+    build(sys.argv[1])
