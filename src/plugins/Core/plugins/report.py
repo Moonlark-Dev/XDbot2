33aa593648753c78a27d2b51ec6a335d4e8a89d1
This is XiaoDeng <104149371+This-is-XiaoDeng@users.noreply.github.com>
2023-02-22 17:50:16 +0800

    [update] 将`report`改为举报（Issue #53）

--- src/plugins/Core/plugins/report.py
+++ src/plugins/Core/plugins/report.py
@@ -4,13 +4,22 @@ from nonebot.params import CommandArg
 from . import _lang
 from . import _error
 
-report = on_command("report", aliases={"反馈"})
+report = on_command("report", aliases={"举报"})
 
 
 @report.handle()
 async def handle(event: MessageEvent, message: Message = CommandArg()):
-    await _error.report(("「新反馈」\n"
-                         f"{message}\n"
-                         f"{event.get_session_id()}\n"))
+    await _error.report(
+        Message((
+            "「举报信息」\n"
+            f"{message}\n"
+            f"{event.get_session_id()}\n")))
     await report.finish(
-        _lang.text("report.github", [], event.get_user_id()), at_sender=True)
+        _lang.text("report.success", [], event.get_user_id()), at_sender=True)
+
+# [HELPSTART] Version: 2
+# Command: report
+# Usage: /report <messages>
+# Msg: 举报其他用户
+# Info: 用于举报其他用户（或自己）的违规行为
+# [HELPEND]
