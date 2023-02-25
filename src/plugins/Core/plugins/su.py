b93f4093fcdcfa2b4d4761e4d60afd623217a4e6
This-is-XiaoDeng <1744793737@qq.com>
2023-02-25 12:21:35 +0800

    [update] 在更新后打印当前版本

--- src/plugins/Core/plugins/su.py
+++ src/plugins/Core/plugins/su.py
@@ -564,6 +564,7 @@ async def suHandle(bot: Bot, event: MessageEvent, message: Message = CommandArg(
             await su.send("正在运行更新程序，请稍候 ...")
             os.system("python3 update.py")
             await su.send("运行完成！")
+            await su.send(os.popen("git log").read().split("\n\n")[0])
         elif argument[0] in ["img", "图库"]:
             data = json.load(open("data/reply.images.json", encoding="utf-8"))
             if argument[1] in ["review", "审核库", "re"]:
