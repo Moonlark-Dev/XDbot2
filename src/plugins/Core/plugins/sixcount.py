1d310a82f4f27b0907177787a639c180acca6159
This is XiaoDeng <104149371+This-is-XiaoDeng@users.noreply.github.com>
2023-03-11 14:53:46 +0800

    [update] Update sixcount.py

--- src/plugins/Core/plugins/sixcount.py
+++ src/plugins/Core/plugins/sixcount.py
@@ -84,7 +84,7 @@ async def pie():
             .set_global_opts(
                 title_opts=opts.TitleOpts(
                     title="6", subtitle=start_time + " 至今"))
-            .set_series_opts(legend_opts=None)
+            .set_series_opts(legend_opts=opts.LegendOpts(is_show=False)))
             .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}"))
         ).render(path="data/sixcount.render.ro.html")
 
