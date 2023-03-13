2d18e9feecd1f8aef9867a3b6d029937d7e7b574
This is XiaoDeng <1744793737@qq.com>
2023-03-13 20:46:12 +0800

    [update] 将cave的冷却缩小为3s

--- src/plugins/Core/plugins/cave.py
+++ src/plugins/Core/plugins/cave.py
@@ -114,8 +114,8 @@ async def cave_handle(bot: Bot, event: MessageEvent, message: Message = CommandA
         data = json.load(open("data/cave.data.json", encoding="utf-8"))
         argument = str(message).split(" ")
         if argument[0] not in ["add", "添加", "-a"] and time.time() - \
-                latest_use < 5:
-            await cave.finish(f"冷却中（{5 - (time.time() - latest_use)}s）", at_sender=True)
+                latest_use < 3:
+            await cave.finish(f"冷却中（{3 - (time.time() - latest_use)}s）", at_sender=True)
         else:
             latest_use = time.time()
         if argument[0] == "":
