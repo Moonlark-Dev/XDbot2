9290b3355ebd32d3d205527bf6594a687a7fd5cd
This is XiaoDeng <1744793737@qq.com>
2023-03-18 20:30:52 +0800

    [add] 查看背包插件

---
+++ src/plugins/Core/plugins/bag.py
@@ -0,0 +1,25 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11.event import MessageEvent
+from nonebot.adapters.onebot.v11.bot import Bot
+from .etm import bag
+from . import _lang as lang
+from . import _error as error
+import traceback
+
+bag = on_command("bag", aliases={"我的背包", "背包"})
+
+@bag.handle()
+async def show_bag(bot: Bot, event: MessageEvent):
+    try:
+        qq = event.get_user_id()
+        data = bag.get_user_bag()
+        nickname = (await bot.get_stranger_info(user_id=qq))["nickname"]
+        reply = f"「{nickname}的背包（{len(data)} / 256）」\n"
+        length = 0
+        for item in data:
+            reply += f"{length}. {item.data['display_name']} x{item.count}\n"
+            length += 1
+        await bag.finish(reply)
+
+    except BaseException:
+        await error.report(traceback.format_exc())
