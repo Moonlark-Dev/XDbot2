15f3a7fd7989aaded63233166bb3c89eddbf3648
This-is-XiaoDeng <1744793737@qq.com>
2023-03-18 14:57:29 +0800

    [update] 对错误上报添加自动过滤

--- src/plugins/Core/plugins/_error.py
+++ src/plugins/Core/plugins/_error.py
@@ -4,18 +4,23 @@
 import json
 from nonebot import get_bots
 import random
+from nonebot.log import logger
 from . import _lang
 
 ctrlGroup = json.load(open("data/ctrl.json"))["control"]
 
 
 async def report(err: str, matcher: any = None):
-    bot = get_bots()[
-        json.load(
-            open("data/su.multiaccoutdata.ro.json"))[ctrlGroup]]
+    error = err.splitlines()[-1]
+    logger.debug(error)
+    if "FinishedException" in error:
+        return None
+    bot = get_bots()[json.load(
+        open("data/su.multiaccoutdata.ro.json"))[ctrlGroup]]
     await bot.send_group_msg(message=err, group_id=ctrlGroup)
+    logger.error(err)
     if matcher is not None:
-        await matcher.send(_lang.text("_error.failed"), at_sender=True)
+        await matcher.send(f"处理失败！\n{error}", at_sender=True)
         if random.random() <= 0.35:
             await matcher.finish(
                 _lang.text(
