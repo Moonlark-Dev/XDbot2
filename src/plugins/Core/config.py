3832db67da206dab49c2350f208af355f41f77db
This is XiaoDeng <1744793737@qq.com>
2023-03-18 12:07:37 +0800

    [add] 新签到模块

--- src/plugins/Core/config.py
+++ src/plugins/Core/config.py
@@ -14,10 +14,30 @@ class Config(BaseSettings):
     DIRECTORIES = [
         "data",
         "data/caveImages",
-        "data/rules"
+        "data/rules",
+        "data/etm"
     ]
     # 数据文件命名规则：子插件.数据名.json
     FILES = [
+        {
+            "path": "data/etm/achievement.json",
+            "text": "{}"
+        },
+        {
+            "path": "data/etm/sign.json",
+            "text": json.dumps({
+                "latest": {},
+                "days": {}
+            })
+        },
+        {
+            "path": "data/etm/users.json",
+            "text": "{}"
+        },
+        {
+            "path": "data/etm/bags.json",
+            "text": "{}"
+        },
         {
             "path": "data/random_events.disable.json",
             "text": json.dumps({
@@ -76,102 +96,15 @@ class Config(BaseSettings):
             })
         },
         {
-            "path": "data/etm.bag.json",
-            "text": "{}"
-        },
-        {
             "path": "data/su.priority_accout.json",
             "text": "{\"accouts\":[]}"
         },
-        {
-            "path": "data/etm.items.json",
-            "text": json.dumps({
-                "0": {
-                    "name": "VimCoin",
-                    "info": "XDbot2 通用货币",
-                    "data": {
-                        "canDrop": False,
-                        "canSell": False
-                    }
-                },
-                "1": {
-                    "name": "FZSGBall",
-                    "info": "球你tm不女装我还不删了！（",
-                    "data": {}
-                },
-                "2": {
-                    "name": "VimCoin礼包",
-                    "info": "来自XDbot某个贡献者的一点小心意\n打开后可获得：\n1. VimCoin x0~50",
-                    "data": {}
-                },
-                "3": {
-                    "name": "二十面骰",
-                    "info": "二十面骰子，致敬LTW3",
-                    "data": {}
-                },
-                "4": {
-                    "name": "书与笔",
-                    "info": "致敬 Minecraft，用于书写文本",
-                    "data": {"author": None, "text": None, "saved": False, "displayName": "书与笔"}
-                },
-                "5": {
-                    "name": "命名牌",
-                    "info": "用于修改物品显示及简介",
-                    "data": {}
-                }
-            })
-        },
-        {
-            "path": "data/sign.latestTime.json",
-            "text": "{}"
-        },
-        {
-            "path": "data/sign.signDay.json",
-            "text": "{}"
-        },
-        {
-            "path": "data/etm.userData.json",
-            "text": "{}"
-        },
+        
         {
             "path": "data/forward.groupList.json",
             "text": "[]"
         },
-        {
-            "path": "data/shop.items.json",
-            "text": json.dumps({
-                # "0": {
-                #    "name": "VimCoin",
-                #    "count": 2,
-                #    "price": 114,
-                #    "seller": {
-                #        "nickname": "StarWorld",
-                #        "user_id": "2915495930"
-                #    },
-                #    "info": "XDbot2 通用货币",
-                #    "item": {
-                #        "id": "0",
-                #        "count": None,
-                #        "data": {}
-                #    }
-                # }
-            })
-        },
-        {
-            "path": "data/autosell.items.json",
-            "text": json.dumps([
-                # {
-                #    "id": "0",
-                #    "count": 5,
-                #    "price": 5,
-                #    "data": {}
-                # }
-            ])
-        },
-        {
-            "path": "data/autosell.latest.json",
-            "text": "{\"mday\":0}"
-        },
+        
         {
             "path": "data/ban.banData.json",
             "text": "{}"
@@ -224,3 +157,42 @@ class Config(BaseSettings):
 
     class Config:
         extra = "ignore"
+
+"""{
+            "path": "data/etm.items.json",
+            "text": json.dumps({
+                "0": {
+                    "name": "VimCoin",
+                    "info": "XDbot2 通用货币",
+                    "data": {
+                        "canDrop": False,
+                        "canSell": False
+                    }
+                },
+                "1": {
+                    "name": "FZSGBall",
+                    "info": "球你tm不女装我还不删了！（",
+                    "data": {}
+                },
+                "2": {
+                    "name": "VimCoin礼包",
+                    "info": "来自XDbot某个贡献者的一点小心意\n打开后可获得：\n1. VimCoin x0~50",
+                    "data": {}
+                },
+                "3": {
+                    "name": "二十面骰",
+                    "info": "二十面骰子，致敬LTW3",
+                    "data": {}
+                },
+                "4": {
+                    "name": "书与笔",
+                    "info": "致敬 Minecraft，用于书写文本",
+                    "data": {"author": None, "text": None, "saved": False, "displayName": "书与笔"}
+                },
+                "5": {
+                    "name": "命名牌",
+                    "info": "用于修改物品显示及简介",
+                    "data": {}
+                }
+            })
+        },"""
