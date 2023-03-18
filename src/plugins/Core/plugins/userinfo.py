ad8c8964220d8f237a7d170e619a95f060b3f51f
This is XiaoDeng <1744793737@qq.com>
2023-03-18 20:51:56 +0800

    [update] 修改用户数据面板排版

--- src/plugins/Core/plugins/userinfo.py
+++ src/plugins/Core/plugins/userinfo.py
@@ -24,7 +24,7 @@ async def show_panel(bot: Bot, event: MessageEvent):
             f"{'-'*27}\n"
             f"{nickname}\n"
             f"  等级：Lv{level} ({int(exp.get_exp(qq))} / {(level+1)**2 - level_max_exp} exp)\n"
-            f"        [{'=' * max(bar_filled-1, 0)}>{'  ' * (10 - bar_filled)}] {int(data['exp'])}exp\n"
+            f"        [{'=' * max(bar_filled-1, 0)}>{'  ' * (10 - bar_filled)}]\n"
             f"  余额：{round(data['vimcoin'], 2)}vim ({round(data['vimcoin'] * economy.vimcoin['exchange_rate'], 2)}vi)\n"
             f"  生命值：{data['health']} / 20"))
 
