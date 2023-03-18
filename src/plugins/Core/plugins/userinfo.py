0d0acd0fc65c362b9b909b96a1dfc4c13fd2ecdd
This is XiaoDeng <1744793737@qq.com>
2023-03-18 21:11:51 +0800

    [fix] 我是傻逼

--- src/plugins/Core/plugins/userinfo.py
+++ src/plugins/Core/plugins/userinfo.py
@@ -17,7 +17,7 @@ async def show_panel(bot: Bot, event: MessageEvent):
         nickname = (await bot.get_stranger_info(user_id=qq))["nickname"]
         level = exp.get_user_level(qq)
         level_max_exp = level ** 2
-        bar_filled = int(exp._get_exp(qq) / level_max_exp * 10)
+        bar_filled = int(exp.get_exp(qq) / (level**2-(level-1)**2) * 10)
 
         await panel.finish((
             "「用户信息面板」\n"
