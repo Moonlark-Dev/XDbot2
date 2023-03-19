849cf2c020fb2a197a45b9d08451405da83bc6c9
This is XiaoDeng <1744793737@qq.com>
2023-03-19 15:21:39 +0800

    [update] 修改用户面板排版

--- src/plugins/Core/plugins/userinfo.py
+++ src/plugins/Core/plugins/userinfo.py
@@ -21,7 +21,7 @@ async def show_panel(bot: Bot, event: MessageEvent):
 
         await panel.finish((
             f"{lang.text('userinfo.title', [], qq)}\n"
-            f"{'-'*27}\n"
+            f"——————————————\n"
             f"{nickname}\n"
             f"  {lang.text('userinfo.level', [], qq)}：Lv{level} ({int(exp.get_exp(qq))} / {(level)**2 - (level-1)**2} exp)\n"
             f"        [{'=' * max(bar_filled-1, 0)}>{'  ' * (10 - bar_filled)}]\n"
