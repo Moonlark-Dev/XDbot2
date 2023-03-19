a65c7390e3fd8b78feb31a7f3a04ddc2268a0c9f
This is XiaoDeng <1744793737@qq.com>
2023-03-19 18:04:22 +0800

    [fix] 修复找不到群聊的问题

--- src/plugins/Core/plugins/upload_logger.py
+++ src/plugins/Core/plugins/upload_logger.py
@@ -5,7 +5,7 @@ import os.path
 import traceback
 from . import _error
 
-upload_log = on_command("su-upload", aliases={"上传日志"})
+upload_log = on_command("upload-log", aliases={"上传日志"})
 
 
 @upload_log.handle()
@@ -13,7 +13,7 @@ async def handle(bot: Bot, event: GroupMessageEvent):
     try:
         await bot.call_api(
             api="upload_group_file",
-            grouo_id=str(event.group_id),
+            group_id=event.group_id,
             file=os.path.abspath('./data/error.log'),
             name="error.log"
         )
