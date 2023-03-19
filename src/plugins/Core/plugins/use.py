c7409afd8ec43353f87360136539de8db2a80c8a
This is XiaoDeng <1744793737@qq.com>
2023-03-19 16:43:44 +0800

    [fix] 修复一个问题

--- src/plugins/Core/plugins/use.py
+++ src/plugins/Core/plugins/use.py
@@ -1,10 +1,10 @@
 from nonebot import on_command
 from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.adapters.onebot.v11.bot import Bot
+# from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.adapters.onebot.v11 import Message
 from nonebot.params import CommandArg
 from .etm import bag
-from . import _lang as lang
+# from . import _lang as lang
 from . import _error as error
 import traceback
 
@@ -14,8 +14,9 @@ use_cmd = on_command("use", aliases={"使用"})
 async def use_item(event: MessageEvent, message: Message = CommandArg()):
     try:
         qq = event.get_user_id()
-        data = bag.get_user_bag(qq)[int(message.extract_plain_text())]
-        await use_cmd.finish(data.use(1))
+        await use_cmd.finish(bag.use_item(
+            qq, int(message.extract_plain_text().split(" ")[0]), 
+            " ".join(message.extract_plain_text().split(" ")[1:])))
 
     except BaseException:
         await error.report(traceback.format_exc(), use_cmd)
