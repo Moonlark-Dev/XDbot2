3a164d2525a3f545c4b72724fbb60a75fbf998c8
This is XiaoDeng <1744793737@qq.com>
2023-03-19 15:23:16 +0800

    [update] 修改用户面板排版

--- src/plugins/Core/plugins/userinfo.py
+++ src/plugins/Core/plugins/userinfo.py
@@ -21,8 +21,8 @@ async def show_panel(bot: Bot, event: MessageEvent):
 
         await panel.finish((
             f"{lang.text('userinfo.title', [], qq)}\n"
-            f"——————————————\n"
-            f"{nickname}\n"
+            f"—————————————\n"
+            f"{nickname}({qq})\n"
             f"  {lang.text('userinfo.level', [], qq)}：Lv{level} ({int(exp.get_exp(qq))} / {(level)**2 - (level-1)**2} exp)\n"
             f"        [{'=' * max(bar_filled-1, 0)}>{'  ' * (10 - bar_filled)}]\n"
             f"  {lang.text('userinfo.vimcoin', [], qq)}：{round(data['vimcoin'], 2)}vim ({round(data['vimcoin'] * economy.vimcoin['exchange_rate'], 2)}vi)\n"
