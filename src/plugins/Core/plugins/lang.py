33fd7f1d8ba64428004095ed2a15fcbc8dbdb052
This is XiaoDeng <1744793737@qq.com>
2023-03-19 09:02:13 +0800

    [update] 允许查看语言详细信息

--- src/plugins/Core/plugins/lang.py
+++ src/plugins/Core/plugins/lang.py
@@ -32,5 +32,29 @@ async def lang_handle(bot: Bot, event: MessageEvent, message: Message = CommandA
             await lang.finish(
                 _lang.text("lang.list", ["\n".join(ls)], event.get_user_id())
             )
+        elif args.split(" ")[0] == "view":
+            lang_name = args.split(" ")[1]
+            if lang_name in _lang._lang_dict.keys():
+                found_key = 0
+                key_length = 0
+                for key in _lang._lang_dict["zh_hans"].keys():
+                    if key not in ["lang.version", "lang.author"]:
+                        if key in _lang._lang_dict[lang_name].keys():
+                            found_key += 1
+                        key_length += 1
+
+                try: version = _lang._lang_dict[lang_name]["lang.version"]
+                except: version = "v1.0.0"
+                try: author = _lang._lang_dict[lang_name]["lang.author"]
+                except: author = "未知创作者"
+
+                await lang.finish("\n".join(
+                    "「语言详细信息」",
+                    f"名称：{lang_name}",
+                    f"版本：{version}",
+                    f"作者：{author}",
+                    f"兼容性：{found_key} / {key_length} {found_key / key_length * 100}%"
+                ))
+
         else:
             await lang.finish(_lang.text("lang.notfound", [args], event.get_user_id()))
