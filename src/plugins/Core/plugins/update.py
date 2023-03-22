7d8a0a735c96c3000cfcb03892624f4fa10fbad7
This is XiaoDeng <1744793737@qq.com>
2023-03-22 18:30:14 +0800

    [update] 为update添加本地化

--- src/plugins/Core/plugins/update.py
+++ src/plugins/Core/plugins/update.py
@@ -1,13 +1,22 @@
 import os
-
+from . import _lang as lang
 from nonebot import on_command
 from nonebot.matcher import Matcher
+from nonebot.adapters.onebot.v11 import MessageEvent
+from . import _error as error
+import traceback
 
 
 @on_command("update", aliases={"检查更新"}).handle()
-async def _(matcher: Matcher):
-    await matcher.send("正在运行更新程序，请稍候 ...")
-    old_branch = os.popen("git log").read().split("\n")[
-        0].split(" ")[1][:7]
-    os.system("python3 update.py")
-    await matcher.send('旧提交：%s\n新提交：%s' % (old_branch, os.popen("git log").read().split("\n")[0].split(" ")[1][:7]))
+async def update_xdbot(matcher: Matcher, event: MessageEvent):
+    try:
+        await matcher.send(lang.text("update.checking", [], event.get_user_id()))
+        old_commit = os.popen("git log").read().split("\n")[
+            0].split(" ")[1][:7]
+        os.system("python3 update.py")
+        await matcher.finish(
+            lang.text("update.finish",
+                      [old_commit, os.popen("git log").read().split("\n")[0].split(" ")[1][:7]]),
+            event.get_user_id())
+    except:
+        await _error.report(traceback.format_exc(), matcher)
