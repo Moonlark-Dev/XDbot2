c8bce56d8ab7501b0a9a07fa88229954080f0895
This is XiaoDeng <1744793737@qq.com>
2023-03-25 23:26:52 +0800

    [update] 加入表达式无法识别提示

--- src/plugins/Core/lua/calc.lua
+++ src/plugins/Core/lua/calc.lua
@@ -1,9 +1,9 @@
 local env = {math = math}
 
 function run_sandbox(code)
-  local func, _ = load("return " .. code, nil, "t", env)
+  local func = load("return " .. code, nil, "t", env)
   if func ~= nil then
     return func()
   end
-  return _
+  return "无法识别的表达式："..code
 end
