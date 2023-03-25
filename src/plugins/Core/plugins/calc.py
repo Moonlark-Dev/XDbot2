f28c6bc8e7605e299b29b38783c7110f759b0b85
一块蒙脱石 <104210832+montmorill@users.noreply.github.com>
2023-03-25 22:16:55 +0800

    Update calc.py

--- src/plugins/Core/plugins/calc.py
+++ src/plugins/Core/plugins/calc.py
@@ -1,21 +1,14 @@
 from nonebot.adapters.onebot.v11 import Message
-from lupa import LuaRuntime
 from nonebot import on_command
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
 from . import _error
 import traceback
 
-lua = LuaRuntime(unpack_returned_tuples=True)
-lua.require("src.plugins.Core.lua.calc")
-run_sandbox = lua.eval("run_sandbox")
-
 
 @on_command("calc", aliases={"计算"}).handle()
 async def run_calc(matcher: Matcher, message: Message = CommandArg()):
     try:
-        await matcher.finish(str(run_sandbox(str(message))))
-
+        await matcher.finish(str(eval(str(message))))
     except BaseException:
         await _error.report(traceback.format_exc(), matcher)
-
