be48d71db84f242d25d6e7f6f644a014e5e7b987
一块蒙脱石 <104210832+montmorill@users.noreply.github.com>
2023-03-26 11:33:28 +0800

    [add] 增加了1小时、今日、总计群聊热度

--- src/plugins/Core/plugins/ghot.py
+++ src/plugins/Core/plugins/ghot.py
@@ -1,8 +1,10 @@
 import json
 from time import time
+from datetime import date
 from nonebot import on_command, on_message
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot
 from nonebot.matcher import Matcher
+from nonebot.params import CommandArg
 from . import _lang as lang
 from . import _error as error
 import traceback
@@ -12,79 +14,125 @@ from nonebot import require
 require("nonebot_plugin_apscheduler")
 
 
+def update_10min():
+    data = json.load(open("data/ghot.10min.json", encoding="utf-8"))
+    for group_id, stamps in data.items():
+        new_stamps = []
+        for stamp in stamps:
+            if time() - stamp < 600:
+                new_stamps.append(stamp)
+        data[group_id] = new_stamps
+    json.dump(data, open("data/ghot.10min.json", "w", encoding="utf-8"))
+    return data
+
+
+def update_hour():
+    data = json.load(open("data/ghot.hour.json", encoding="utf-8"))
+    for group_id, stamps in data.items():
+        new_stamps = []
+        for stamp in stamps:
+            if time() - stamp < 600:
+                new_stamps.append(stamp)
+        data[group_id] = new_stamps
+    json.dump(data, open("data/ghot.hour.json", "w", encoding="utf-8"))
+
+
 @scheduler.scheduled_job("cron", second="*/15", id="update_groups_data")
-async def clean_data():
-    data = json.load(open("data/ghot.data.json", encoding="utf-8"))
-    now_time = time()
-    for group in list(data.keys()):
-        _add = 0
-        for l in range(len(data[group])):
-            if now_time - data[group][l - _add] >= 600:
-                data[group].pop(l - _add)
-                _add += 1
-    json.dump(data, open("data/ghot.data.json", "w", encoding="utf-8"))
+async def _():
+    update_10min()
+    update_hour()
 
 
 @on_command("ghot", aliases={"群聊热度"}).handle()
-async def ghot(bot: Bot, event: GroupMessageEvent, matcher: Matcher):
+async def _(bot: Bot, event: GroupMessageEvent, matcher: Matcher,
+            arg=str(CommandArg()).replace("-", "")):
     try:
-        data = json.load(open("data/ghot.data.json", encoding="utf-8"))
-        # Clean Data
-        now_time = time()
-        for group in list(data.keys()):
-            _add = 0
-            for l in range(len(data[group])):
-                if now_time - data[group][l - _add] >= 600:
-                    data[group].pop(l - _add)
-                    _add += 1
-        # 排序
-        groups = []
-        for group in list(data.keys()):
-            hot = len(data[group])
-            if hot == 0:
-                continue
-            is_inserted = False
-            for i in range(len(groups)):
-                if groups[i]["hot"] <= hot:
-                    groups.insert(i, {"group": group, "hot": hot})
-                    is_inserted = True
-                    break
-            if not is_inserted:
-                groups.append({"group": group, "hot": hot})
-        # 赋排名
-        now_rank = 0
-        group_count_in_rank = 1
-        now_rank_hot = -1
-        this_group_rank = -1
-        for i in range(len(groups)):
-            if groups[i]["hot"] == now_rank_hot:
-                groups[i]["rank"] = now_rank
-                group_count_in_rank += 1
-            else:
-                now_rank_hot = groups[i]["hot"]
-                now_rank += group_count_in_rank
-                group_count_in_rank = 1
-                groups[i]["rank"] = now_rank
-            if groups[i]["group"] == str(event.group_id):
-                this_group_rank = now_rank
-        # 输出
-        reply = lang.text("ghot.title", [], event.get_user_id())
-        for group in groups[:10]:
-            reply += f"{group['rank']}. {(await bot.get_group_info(group_id=group['group']))['group_name']}: {group['hot']}\n"
-        await matcher.finish(reply[:-1])
+        if arg in ["", "m", "min"]:
+            reply = lang.text("ghot.10min", [], event.get_user_id())
+            data = update_10min()
+            key = lambda x: len(x[1])
+
+        elif arg in ["h", "hour"]:
+            reply = lang.text("ghot.hour", [], event.get_user_id())
+            data = update_hour()
+            key = lambda x: len(x[1])
+
+        elif arg in ["d", "day"]:
+            reply = lang.text("ghot.day", [], event.get_user_id())
+            data = json.load(open("data/ghot.day.json", encoding="utf-8"))
+            key = lambda x: x[1]
+
+        elif arg in ["t", "total"]:
+            reply = lang.text("ghot.total", [], event.get_user_id())
+            data = json.load(open("data/ghot.total.json", encoding="utf-8"))
+            key = lambda x: x[1]
+
+        else:
+            matcher.finish()
+
+        sorted_data = sorted(data.items(), key=key, reverse=True)
+        for i in range(min(10, len(sorted_data))):
+            group_id = sorted_data[i][0]
+            count = sorted_data[group_id]
+            group_name = (await bot.get_group_info(group_id=int(group_id)))["group_name"]
+            reply += f"{i + 1}. {group_name}: {count}\n"
+        reply += "-" * 30 + "\n"
+        group_id = event.get_user_id()
+        count = sorted_data[group_id]
+        for i in range(len(sorted_data)):
+            if sorted_data[i][0] == group_id:
+                group_name = (await bot.get_group_info(group_id=int(group_id)))["group_name"]
+                reply += f"{i + 1}. {group_name}: {count}\n"
+                break
+        await matcher.finish(reply)
 
     except BaseException:
         await error.report(traceback.format_exc(), matcher)
 
 
 @on_message().handle()
-async def ghot_writer(event: GroupMessageEvent):
+async def _(event: GroupMessageEvent):
     try:
-        data = json.load(open("data/ghot.data.json", encoding="utf-8"))
-        try:
-            data[str(event.group_id)].append(time())
-        except KeyError:
-            data[str(event.group_id)] = [time()]
-        json.dump(data, open("data/ghot.data.json", "w", encoding="utf-8"))
+        # 10min
+        data = json.load(open("data/ghot.10min.json", encoding="utf-8"))
+        if str(event.group_id) not in data.keys():
+            data[str(event.group_id)] = []
+        data[str(event.group_id)].append(time())
+        json.dump(data, open("data/ghot.10min.json", "w", encoding="utf-8"))
+
+        # hour
+        data = json.load(open("data/ghot.hour.json", encoding="utf-8"))
+        if str(event.group_id) not in data.keys():
+            data[str(event.group_id)] = []
+        data[str(event.group_id)].append(time())
+        json.dump(data, open("data/ghot.hour.json", "w", encoding="utf-8"))
+
+        # day
+        data = json.load(open("data/ghot.day.json", encoding="utf-8"))
+        if ("date" not in data.keys() or
+                data["date"] != str(date.today())):
+            data = {"date": str(date.today())}
+        if str(event.group_id) not in data.keys():
+            data[str(event.group_id)] = 0
+        data[str(event.group_id)] += 1
+        json.dump(data, open("data/ghot.day.json", "w", encoding="utf-8"))
+
+        # total
+        data = json.load(open("data/ghot.total.json", encoding="utf-8"))
+        if str(event.group_id) not in data.keys():
+            data[str(event.group_id)] = 0
+        data[str(event.group_id)] += 1
+        json.dump(data, open("data/ghot.total.json", "w", encoding="utf-8"))
+
     except BaseException:
         await error.report(traceback.format_exc())
+
+# [HELPSTART] Version: 2
+# Command: ghot
+# Usage: ghot：获取最近10分钟的群聊热度
+# Usage: ghot-h：获取最近1小时的群聊热度
+# Usage: ghot-d：获取今日的群聊热度
+# Usage: ghot-t：获取总计的群聊热度
+# Info: 获取最近10分钟、最近1小时、今日或总计的群聊热度
+# Msg: 获取群聊热度
+# [HELPEND]
