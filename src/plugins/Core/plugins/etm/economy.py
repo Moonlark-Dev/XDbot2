be0f29e5290e3a3dfcbc78ea0e588a69a6bca621
This-is-XiaoDeng <1744793737@qq.com>
2023-03-26 12:23:55 +0800

    [update] 修改汇率算法

--- src/plugins/Core/plugins/etm/economy.py
+++ src/plugins/Core/plugins/etm/economy.py
@@ -31,7 +31,7 @@ async def change_exchange_rate():
     all_vi = all_vimcoin * vimcoin["exchange_rate"]
     temp = all_vi / len(list(data.keys()))
     vi = 1000
-    vimcoin["exchange_rate"] -= temp / vi / 1500
+    vimcoin["exchange_rate"] -= temp / vi / 1350
     json.dump(vimcoin, open("data/etm/vimcoin.json", "w", encoding="utf-8"))
 
 def _add_vimcoin(user_id, count):
