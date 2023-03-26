141861699954517bef123a5c95879fb6aeb7a21d
This-is-XiaoDeng <1744793737@qq.com>
2023-03-26 12:49:21 +0800

    [fix] 修复已知问题

--- src/plugins/Core/plugins/quick_math.py
+++ src/plugins/Core/plugins/quick_math.py
@@ -18,21 +18,21 @@ answer = None
 async def delete_msg(bot, message_id):
     global group, answer
     await asyncio.sleep(10)
-    if None in [group, answer]:
-        await bot.delete_msg(message_id)
+    if None not in [group, answer]:
+        await bot.delete_msg(message_id=message_id)
         group = None
         answer = None
 
-@scheduler.scheduled_job("cron", minute="*/10", id="send_quick_math")
+@scheduler.scheduled_job("cron", second="*/300", id="send_quick_math")
 async def send_quick_math():
     global group, answer
     try:
         accout_data = json.load(open("data/su.multiaccoutdata.ro.json", encoding="utf-8"))
         groups = json.load(open("data/quick_math.enabled_groups.json", encoding="utf-8"))
         group = random.choice(groups)
-        question = f"{random.randint(0, 100)} {random.choice('+-*/')} {random.randint(1, 100)}"
+        question = f"{random.randint(0, 50)} {random.choice('+-*/')} {random.randint(1, 50)}"
         answer = eval(question)
-        bot = get_bot(str(accout_data[group]))
+        bot = get_bot(accout_data[str(group)])
         msg_id = (await bot.send_group_msg(
             group_id = group,
             message = f"[QUICK MATH] {question} = ?"))["message_id"]
@@ -54,10 +54,10 @@ async def quick_math(matcher: Matcher, event: GroupMessageEvent):
                 add = [random.randint(1, 13), random.randint(1, 15)]
                 economy.add_vi(event.get_user_id(), add[0])
                 exp.add_exp(event.get_user_id(), add[1])
-                await matcher.finish(lang.text("quick_math.rightanswer", add, event.get_user_id()),
+                await matcher.send(lang.text("quick_math.rightanswer", add, event.get_user_id()),
                                      at_sender=True)
-            group = None
-            answer = None
+                group = None
+                answer = None
 
     except BaseException:
         await error.report(format_exc())
