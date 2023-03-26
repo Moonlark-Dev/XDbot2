0ad1f98cec17ec7d758145e418ae06a93bc7d89d
This is XiaoDeng <1744793737@qq.com>
2023-03-26 18:57:58 +0800

    [update] 修改汇率算法

--- src/plugins/Core/plugins/etm/economy.py
+++ src/plugins/Core/plugins/etm/economy.py
@@ -17,7 +17,7 @@ class IllegalQuantityException(Exception): pass
 async def change_exchange_rate():
     # 一阶段
     exchange_rate = vimcoin["exchange_rate"]
-    vimcoin["exchange_rate"] += (vimcoin["out"] - vimcoin["in"]) / 2000
+    vimcoin["exchange_rate"] += (vimcoin["out"] - vimcoin["in"]) / 1500
     vimcoin["in"] = 0
     vimcoin["out"] = 0
     if vimcoin["exchange_rate"] <= 0:
