7a9346a806587561c6bcd9525454b77308341b7a
This is XiaoDeng <1744793737@qq.com>
2023-04-01 23:30:53 +0800

    [update] 发送版本时发送提交信息

--- src/plugins/Core/plugins/version.py
+++ src/plugins/Core/plugins/version.py
@@ -9,7 +9,7 @@ git_log = os.popen("git log").read()
 @on_command("version", aliases={"版本信息"}).handle()
 async def show_version(matcher: Matcher):
     try:
-        await matcher.finish(git_log.split("\n\n")[0])
+        await matcher.finish(git_log.split("\n\ncommit")[0])
     except BaseException:
         await error.report(traceback.format_exc(), matcher)
 
