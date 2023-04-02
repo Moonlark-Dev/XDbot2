f4702b0c7dd4eed0180ca338f259eb5ff2407062
一块蒙脱石 <104210832+montmorill@users.noreply.github.com>
2023-04-02 08:29:44 +0800

    [update] 消灭浮点误差，世界属于Decimal！

--- src/plugins/Core/plugins/sign.py
+++ src/plugins/Core/plugins/sign.py
@@ -1,5 +1,4 @@
-from nonebot import on_regex
-from nonebot import on_command
+from nonebot import on_command, on_fullmatch
 from . import _error as error
 from nonebot.adapters.onebot.v11.event import MessageEvent
 from . import _lang as lang
@@ -9,7 +8,9 @@ import random
 import time
 import json
 
-sign = on_regex(r"^(签到|(.sign))$")
+from decimal import Decimal
+
+sign = on_fullmatch(("签到", ".sign"))
 sign_rank = on_command("sign-rank")
 
 
@@ -31,16 +32,16 @@ async def sign_handler(event: MessageEvent):
             else:
                 data["days"][qq] = 1
 
-            add_vi *= 1 + min(data["days"][qq], 15) / 20
-            add_exp *= 1 + min(data["days"][qq], 15) / 20
-            add_vi *= 1 + exp.get_user_level(qq) / 75
-            add_exp *= 1 + exp.get_user_level(qq) / 50
-            add_vi /= 1.2
+            add_vi *= 1 + min(data["days"][qq], 15) / Decimal(20)
+            add_exp *= 1 + min(data["days"][qq], 15) / Decimal(20)
+            add_vi *= 1 + exp.get_user_level(qq) / Decimal(75)
+            add_exp *= 1 + exp.get_user_level(qq) / Decimal(50)
+            add_vi /= Decimal("1.2")
             add_vi = round(add_vi, 3)
             add_exp = round(add_exp, 0)
 
-            exp.add_exp(qq, add_exp)
-            economy.add_vi(qq, add_vi)
+            exp.add_exp(qq, float(add_exp))
+            economy.add_vi(qq, int(add_vi))
             data["latest"][qq] = date
             now_data = user.get_user_data(qq)
             await sign.send("\n".join((
@@ -48,15 +49,15 @@ async def sign_handler(event: MessageEvent):
                 lang.text("sign.hr", [], qq),
                 lang.text(
                     "sign.add_exp", [
-                        origin_data["exp"], now_data["exp"], add_exp], qq),
+                        round(origin_data["exp"], 2), round(now_data["exp"], 2), add_exp], qq),
                 lang.text(
                     "sign.add_vim", [
-                        origin_data["vimcoin"], now_data["vimcoin"], add_vi], qq),
+                        round(origin_data["vimcoin"], 2), round(now_data["vimcoin"], 2), add_vi], qq),
                 lang.text("sign.hr", [], qq),
                 lang.text("sign.days", [data["days"][qq]], qq)
             )), at_sender=True)
             json.dump(data, open("data/etm/sign.json", "w", encoding="utf-8"))
-            if add_vi == 0:
+            if add_vi == Decimal(0):
                 achievement.unlck("+0！", event.get_user_id())
 
     except BaseException:
