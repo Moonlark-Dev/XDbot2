ecaacbeb5ad022476643c3ea90b46c2bf081bfb3
This is XiaoDeng <1744793737@qq.com>
2023-04-02 20:37:03 +0800

    [update] 修改更新程序

--- XDbot2_Update.py
+++ XDbot2_Update.py
@@ -6,9 +6,11 @@ import json
 print("欢迎使用：XDbot2 v2.7.37")
 
 data = json.load(open("data/cave.data.json", "r", encoding="utf-8"))
-
+# print(data)
 for key in list(data["data"].keys()):
-    if str(data["data"][key]["sender"]) == "269651975":
+    print(key, data["data"][key]["sender"])
+    if data["data"][key]["sender"] == "2696519745":
         data["data"].pop(key)
+        print("removed", key)
 
 json.dump(data, open("data/cave.data.json", "w", encoding="utf-8"))
