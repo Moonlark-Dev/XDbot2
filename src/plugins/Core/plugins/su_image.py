0fef395fdf3f1f4ae237bdd683af6d94cf74b913
This is XiaoDeng <1744793737@qq.com>
2023-04-02 12:25:06 +0800

    [update] 重构 `su img`
    参考 Issue #118

---
+++ src/plugins/Core/plugins/su_image.py
@@ -0,0 +1,70 @@
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import Message
+from .su import su
+from . import _error
+import json
+import traceback
+
+
+@su.handle()
+async def img_review(argument: list = str(CommandArg()).split(" ")):
+    try:
+        if argument[0] in ["img", "图库"]:
+            data = json.load(open("data/reply.images.json", encoding="utf-8"))
+            if argument[1] in ["review", "审核库", "re"]:
+                if argument[2] in ["list", "列表"]:
+                    for key in data["review"].keys():
+                        await su.send(Message(f"「ID：{key}」\n{data['review'][key]}"))
+                elif argument[2] in ["pass", "通过"]:
+                    if argument[3] in ["all", "所有", "*"]:
+                        for key in list(data["review"].keys()):
+                            data["B"].append(data["review"].pop(key))
+                    else:
+                        if len(argument) >= 5:
+                            group = argument[4]
+                        else:
+                            group = "B"
+                        tempID = len(data[group])
+                        image = data["review"].pop(argument[3])
+                        data[group].append(image)
+                        await su.send(Message(f"「图片已添加」\n临时ID：{group}{tempID}"))
+                elif argument[2] in ["remove", "删除", "rm"]:
+                    if argument[3] in ["all", "所有", "*"]:
+                        data["review"] = dict()
+                    else:
+                        data["review"].pop(argument[3])
+            json.dump(
+                data,
+                open(
+                    "data/reply.images.json",
+                    "w",
+                    encoding="utf-8"))
+    except BaseException:
+        await _error.report(traceback.format_exc(), su)
+
+
+@su.handle()
+async def image(argument: list = str(CommandArg()).split(" ")):
+    try:
+        if argument[0] in ["img", "图库"] and argument[1] not in [
+                "review", "re", "审核库"]:
+            data = json.load(open("data/reply.images.json", encoding="utf-8"))
+            if argument[1] in ["添加", "add"]:
+                data[argument[2]].append(argument[3])
+            elif argument[1] in ["list", "列表"]:
+                length = 0
+                for image in data[argument[2]]:
+                    await su.send(Message(f"「临时ID：{argument[2]}{length}」\n{image}"))
+                    length += 1
+            elif argument[1] in ["remove", "删除"]:
+                data[argument[2]].pop(int(argument[3]))
+            elif argument[1] in ["clear", "清空"]:
+                data = {"A": [], "B": [], "C": [], "review": dict()}
+            json.dump(
+                data,
+                open(
+                    "data/reply.images.json",
+                    "w",
+                    encoding="utf-8"))
+    except BaseException:
+        await _error.report(traceback.format_exc(), su)
--- src/plugins/Core/plugins/su_old.py
+++ src/plugins/Core/plugins/su_old.py
@@ -469,48 +469,7 @@ async def suHandle(bot: Bot, event: MessageEvent, message: Message = CommandArg(
             elif argument[1] in ["remove", "rm", "删除"]:
                 smart_reply.remove_reply(
                     argument[2], event.get_user_id(), True)
-        elif argument[0] in ["img", "图库"]:
-            data = json.load(open("data/reply.images.json", encoding="utf-8"))
-            if argument[1] in ["review", "审核库", "re"]:
-                if argument[2] in ["list", "列表"]:
-                    for key in data["review"].keys():
-                        await su.send(Message(f"「ID：{key}」\n{data['review'][key]}"))
-                elif argument[2] in ["pass", "通过"]:
-                    if argument[3] in ["all", "所有", "*"]:
-                        for key in list(data["review"].keys()):
-                            data["B"].append(data["review"].pop(key))
-                    else:
-                        if len(argument) >= 5:
-                            group = argument[4]
-                        else:
-                            group = "B"
-                        tempID = len(data[group])
-                        image = data["review"].pop(argument[3])
-                        data[group].append(image)
-                        await su.send(Message(f"「图片已添加」\n临时ID：{group}{tempID}"))
-                elif argument[2] in ["remove", "删除", "rm"]:
-                    if argument[3] in ["all", "所有", "*"]:
-                        data["review"] = dict()
-                    else:
-                        data["review"].pop(argument[3])
-            elif argument[1] in ["添加", "add"]:
-                data[argument[2]].append(argument[3])
-            elif argument[1] in ["list", "列表"]:
-                length = 0
-                for image in data[argument[2]]:
-                    await su.send(Message(f"「临时ID：{argument[2]}{length}」\n{image}"))
-                    length += 1
-            elif argument[1] in ["remove", "删除"]:
-                data[argument[2]].pop(int(argument[3]))
-            elif argument[1] in ["clear", "清空"]:
-                data = {"A": [], "B": [], "C": [], "review": dict()}
-            json.dump(
-                data,
-                open(
-                    "data/reply.images.json",
-                    "w",
-                    encoding="utf-8"))
-        elif argument[0] in ["ma", "multiaccout", "多账户"]:
+               elif argument[0] in ["ma", "multiaccout", "多账户"]:
             if argument[1] in ["status", "状态"]:
                 reply = "「XDbot2 Multi-Accout 账户列表」\n"
                 bots = get_bots()
