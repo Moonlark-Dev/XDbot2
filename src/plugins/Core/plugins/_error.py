49303a3edcbebcdd461f7fe51bf43d0c4745b0bf
This is XiaoDeng <1744793737@qq.com>
2023-04-02 20:23:27 +0800

    [update] 上报时过滤`FinishedException`
    参考 Issue #120

--- src/plugins/Core/plugins/_error.py
+++ src/plugins/Core/plugins/_error.py
@@ -9,13 +9,21 @@ from nonebot.log import logger
 from . import _lang
 
 ctrlGroup = json.load(open("data/ctrl.json", encoding="utf-8"))["control"]
+IGNORED_EXCEPTION = [
+    "NewWorkError"
+]
 
 
 async def report(err: str, matcher: any = None):
     error = err.splitlines()[-1]
     logger.debug(error)
+    # 过滤错误
     if "FinishedException" in error:
         raise FinishedException()
+    for e in IGNORED_EXCEPTION:
+        if e in error: # Issue #120
+            return None
+    # 上报错误
     bot = get_bots()[json.load(
         open("data/su.multiaccoutdata.ro.json", encoding="utf-8"))[ctrlGroup]]
     await bot.send_group_msg(message=err, group_id=ctrlGroup)
