007de098c83e4bbf4cbd850ef6efc7bad49d3bfa
This is XiaoDeng <1744793737@qq.com>
2023-04-02 12:42:00 +0800

    [update] 支持使用`su config get`访问列表
    从此版本开始，参数`[key]`的类型将被修改为`json`
    
    读取字符串键时需要带上`""`

--- src/plugins/Core/plugins/su_config.py
+++ src/plugins/Core/plugins/su_config.py
@@ -8,12 +8,12 @@ import traceback
 async def get_config(argument: list = str(CommandArg()).split(" ")):
     try:
         if argument[0] in ["config", "配置"]:
-            if argument[1] == "get" or argument[1] == "获取":
+            if argument[1] in ["get", "获取"]:
                 if len(argument) >= 4:
                     await su.finish(
                         json.dumps(
                             json.load(open(f"data/{argument[2]}", encoding="utf-8"))[
-                                " ".join(argument[3:])
+                                json.loads(" ".join(argument[3:]))
                             ]
                         )
                     )
