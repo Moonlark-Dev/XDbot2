f24255a05fa7c0c520d477f7dbd956bee1090fd8
This is XiaoDeng <1744793737@qq.com>
2023-04-08 22:36:29 +0800

    [update] 修改护符生效时的提示文本

--- src/plugins/Core/plugins/etm/dice.py
+++ src/plugins/Core/plugins/etm/dice.py
@@ -35,7 +35,7 @@ class Dice(Item):
             achievement.increase_unlock_progress("什么欧皇", user_id)
             if buff.can_effect(user_id, "护符") and random.random() <= 0.75:
                 buff.effect_buff(user_id, "护符")
-                return f"你买了一个二十面骰子，掷出了 {c}，护符已生效"
+                return f"你买了一个二十面骰子，掷出了 {c}，大失败！护符生效，扣款已取消"
             else:
                 economy._add_vi(user_id, -50)
                 return f"你买了一个二十面骰子，掷出了 {c}，大失败！倾家荡产，丢失了50vi！"
