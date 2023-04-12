4604a11d28e3130a08f049b0d02dd91bff04dd7d
This is XiaoDeng <1744793737@qq.com>
2023-04-12 19:15:29 +0800

    [update] 添加部分内置API

--- src/plugins/Core/plugins/chatgpt.py
+++ src/plugins/Core/plugins/chatgpt.py
@@ -17,18 +17,24 @@ openai.proxy = "http://127.0.0.1:7890"
 @get_driver().on_startup
 async def get_apikeys():
     global api_keys
-    while True:
+    for i in range(5):
         try:
             async with httpx.AsyncClient(proxies=openai.proxy) as client:
                 response = await client.get("http://freeopenai.xyz/api.txt")
             api_keys = response.read().decode("utf-8").splitlines()
             logger.info(f"成功获取 {len(api_keys)} 个 API 密钥")
-            break
+            return None
         except:
-            logger.warning(f"获取 API 秘钥失败，将在 5s 后重试")
+            logger.warning(f"获取 API 秘钥失败，将在 5s 后重试（{i} / 5）")
             await asyncio.sleep(5)
-            
-
+    logger.warning("无法获取 API 秘钥，使用内置")
+    api_keys = [
+        "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",
+        "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",
+        "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",
+        "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",
+        "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"
+    ]
 
 @on_command("gpt", aliases={"chat", "chatgpt"}).handle()
 async def _(matcher: Matcher, event: GroupMessageEvent, message: Message = CommandArg()):
