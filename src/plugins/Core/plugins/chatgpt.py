ef123b9374e523c23505ff15653b1f68b9475fec
This is XiaoDeng <1744793737@qq.com>
2023-04-12 19:26:12 +0800

    [fix] 修复已知问题

--- src/plugins/Core/plugins/chatgpt.py
+++ src/plugins/Core/plugins/chatgpt.py
@@ -8,8 +8,6 @@ from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
 import asyncio
 
-
-api_keys = []
 messages = json.load(open("data/chatgpt.messages.json", encoding="utf-8"))
 openai.proxy = "http://127.0.0.1:7890"
 openai.api_key = "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"
@@ -20,7 +18,6 @@ async def _(matcher: Matcher, event: GroupMessageEvent, message: Message = Comma
         if str(event.group_id) not in messages.keys():
             messages[str(event.group_id)] = []
         messages[str(event.group_id)].append({"role": "user", "content": message.extract_plain_text()})
-        openai.api_key = choice(api_keys)
         session = await openai.ChatCompletion.acreate(
             model="gpt-3.5-turbo",
             messages=messages[str(event.group_id)])
