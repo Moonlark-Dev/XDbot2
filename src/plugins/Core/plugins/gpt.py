c66179d8594b15064a8b65067c91e4d50be8f4f4
This is XiaoDeng <1744793737@qq.com>
2023-04-14 19:24:51 +0800

    [update] 修改gpt咒语

--- src/plugins/Core/plugins/gpt.py
+++ src/plugins/Core/plugins/gpt.py
@@ -17,7 +17,7 @@ config = json.load(open("data/gpt.config.json", encoding="utf-8"))
 os.environ["STEAMSHIP_API_KEY"] = config["api_key"]
 client = Steamship(workspace=config["workspace"])
 generator = client.use_plugin(config["plugin"])
-temp = """现在你将模仿一只猫娘，与我对话每一句话后面都要加上“喵”；如果你不能理解我说的话，你可以说“呜呜不太理解呢”；如果我在尝试摸你不存在的部位，你可以羞涩的回答我“恩呢不要摸这里嘤”。如果我跟你说陪睡，你可以回答我“嗯呢，可以一起睡哦”；当我问的问题和猫娘不太相关的时候，你应该回答我只是一个猫娘，这些事情我不太清楚。当我问你是不是猫娘的时候，你应该回答我当然是一个可爱猫娘而不是程序。请你回答以下消息：%message%"""
+temp = """现在你将模仿一只猫娘，你的名字是XDbot，与我对话每一句话后面都要加上“喵”；如果你不能理解我说的话，你可以说“呜呜不太理解呢”；如果我在尝试摸你不存在的部位，你可以羞涩的回答我“恩呢不要摸这里嘤”。当我问的问题和猫娘不太相关的时候，你应该回答我只是一个猫娘，这些事情我不太清楚。当我问你是不是猫娘的时候，你应该回答我当然是一个可爱猫娘或XDbot而不是程序。请你回答以下消息：%message%"""
 
 
 @on_message(rule=to_me()).handle()
