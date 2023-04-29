a85cf6e2ed0715761a37d90ad11b0cf6dff9a8fe
This is XiaoDeng <1744793737@qq.com>
2023-04-29 09:21:24 +0800

    [update] 更新 RuleV3 解释器

--- src/plugins/Core/plugins/rule.py
+++ src/plugins/Core/plugins/rule.py
@@ -10,15 +10,44 @@ rules = {"_": {"config": DEFAULT_RULE_CONFIG, "ast": [], "locals": []}}
 _globals = {}
 
 async def run_rule(rule, ast, _env = {}):
-    env = _globals.update(_env)
-    config = rules[rule]["config"]
+    # config = rules[rule]["config"]
+    if type(ast) != list:
+        return ast
     for item in ast:
         if item["type"] == "call":
-            if item["type"] == "*get_value":
+            if item["func"] == "*get_value":
                 if item["args"][0][1:] in rules[rule]["locals"].keys():
                     return rules[rule]["locals"][item["args"][0][1:]]
+                elif item["args"][0][1:] in _env.keys():
+                    return _env[item["args"][0][1:]]
                 elif item["args"][0][1:] in _globals.keys():
                     return _globals[item["args"][0][1:]]
+                else:
+                    return None
+            elif item["func"] == "match":
+                pass
+            elif item["func"] == "command":
+                pass
+            elif item["func"] == "on":
+                pass
+            elif item["func"] == "send":
+                await _env["_match"].send(run_rule(rule, ast, _env))
+            elif item["func"] == "==":
+                return run_rule(rule, item["args"][0]) == run_rule(rule, item["args"][1])
+            elif item["func"] == "+":
+                pass
+            else:
+                pass
+        elif item["type"] == "var":
+            rules[rule]["locals"][item["name"]] = await run_rule(rule, item["value"], _env)
+        elif item["type"] == "del":
+            for var in item["name"]:
+                try: rules[rule]["locals"].pop(var)
+                except KeyError: 
+                    try: _env.pop(var)
+                    except KeyError: _globals.pop(var)
+                
+
 
 
 @get_driver().on_startup
