725126869ab3ec0063e76e96a129c8bb9d6ba69a
This is XiaoDeng <1744793737@qq.com>
2023-04-29 13:04:18 +0800

    [update] 将 if-else 改为 match-case

--- src/plugins/Core/plugins/rule.py
+++ src/plugins/Core/plugins/rule.py
@@ -14,38 +14,54 @@ async def run_rule(rule, ast, _env = {}):
     if type(ast) != list:
         return ast
     for item in ast:
-        if item["type"] == "call":
-            if item["func"] == "*get_value":
-                if item["args"][0][1:] in rules[rule]["locals"].keys():
-                    return rules[rule]["locals"][item["args"][0][1:]]
-                elif item["args"][0][1:] in _env.keys():
-                    return _env[item["args"][0][1:]]
-                elif item["args"][0][1:] in _globals.keys():
-                    return _globals[item["args"][0][1:]]
-                else:
-                    return None
-            elif item["func"] == "match":
-                pass
-            elif item["func"] == "command":
-                pass
-            elif item["func"] == "on":
-                pass
-            elif item["func"] == "send":
-                await _env["_match"].send(run_rule(rule, ast, _env))
-            elif item["func"] == "==":
-                return run_rule(rule, item["args"][0]) == run_rule(rule, item["args"][1])
-            elif item["func"] == "+":
-                pass
-            else:
-                pass
-        elif item["type"] == "var":
-            rules[rule]["locals"][item["name"]] = await run_rule(rule, item["value"], _env)
-        elif item["type"] == "del":
-            for var in item["name"]:
-                try: rules[rule]["locals"].pop(var)
-                except KeyError: 
-                    try: _env.pop(var)
-                    except KeyError: _globals.pop(var)
+        match item["type"]:
+            case "call":
+                match item["func"]:
+                    case "*get_value":
+                        if item["args"][0][1:] in rules[rule]["locals"].keys():
+                            return rules[rule]["locals"][item["args"][0][1:]]
+                        elif item["args"][0][1:] in _env.keys():
+                            return _env[item["args"][0][1:]]
+                        elif item["args"][0][1:] in _globals.keys():
+                            return _globals[item["args"][0][1:]]
+                        else:
+                            return None
+                    case "match":
+                        pass
+                    case "command":
+                        pass
+                    case "on":
+                        pass
+                    case "send":
+                        await _env["_match"].send(run_rule(rule, ast, _env))
+                    case "==":
+                        return run_rule(rule, item["args"][0]) == run_rule(rule, item["args"][1])
+                    case "+":
+                        pass
+                    case "-":
+                        pass
+                    case "*":
+                        pass
+                    case "/":
+                        pass
+                    case "format":
+                        pass
+                    case "++":
+                        pass
+            case "var":
+                rules[rule]["locals"][item["name"]] = await run_rule(rule, item["value"], _env)
+            case "del":
+                for var in item["name"]:
+                    try:
+                        rules[rule]["locals"].pop(var)
+                    except KeyError: 
+                        try:
+                            _env.pop(var)
+                        except KeyError:
+                            try:
+                                _globals.pop(var)
+                            except KeyError:
+                                pass
                 
 
 
