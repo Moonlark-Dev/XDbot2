f8b8e40ee58d2a560bbea0dd3f7d5006b4d99643
This is XiaoDeng <1744793737@qq.com>
2023-04-30 22:35:17 +0800

    [update] 修改非su邀请加群时的提示

--- src/plugins/Core/plugins/auto_approve.py
+++ src/plugins/Core/plugins/auto_approve.py
@@ -18,7 +18,7 @@ async def group_request_handle(
                 await event.approve(bot)
             else:
                 _messenger.send_message(
-                    f"温馨提示：\n请在 https://github.com/ITCraftDevelopmentTeam/XDbot2/issues/new?template=group.yml 发送一个入群申请来部署 XDbot2 到 {event.group_id}\n \n详细信息：\nhttps://github.com/ITCraftDevelopmentTeam/XDbot2/discussions/176",
+                    f" 『温馨提示』\n请在 https://github.com/ITCraftDevelopmentTeam/XDbot2/issues/new?template=group.yml 发送一个入群申请来部署 XDbot2 到 {event.group_id}\n \n详细信息：\nhttps://github.com/ITCraftDevelopmentTeam/XDbot2/discussions/176",
                     event.get_user_id()
                 )
 
