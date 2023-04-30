ec11e6c3e91a07b62db2d81cedb534aa19b03863
This is XiaoDeng <1744793737@qq.com>
2023-04-30 13:28:26 +0800

    [update] 缩短 quickmath 触发间隔

--- src/plugins/Core/plugins/quick_math.py
+++ src/plugins/Core/plugins/quick_math.py
@@ -85,7 +85,7 @@ async def delete_msg(bot, message_id):
         answer = None
 
 
-@scheduler.scheduled_job("cron", minute="*/3", id="send_quick_math")
+@scheduler.scheduled_job("cron", minute="*/2", id="send_quick_math")
 async def send_quick_math():
     global group, answer, send_time
     try:
