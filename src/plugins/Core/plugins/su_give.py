41f4d3db17163a21aeba422876194ae1d38f133c
This is XiaoDeng <1744793737@qq.com>
2023-06-23 13:15:31 +0800

    [add] 添加 `su give` 子命令

---
+++ src/plugins/Core/plugins/su_give.py
@@ -0,0 +1,22 @@
+from .su import su
+from . import _error
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+from .etm import bag
+
+try:
+    import json5 as json
+except:
+    import json
+
+@su.handle()
+async def _(message: Message = CommandArg()):
+    try:
+        args = str(message).strip().split(" ")
+        if args[0] in ["give", "给"]:
+            bag.add_item(args[1].replace("[CQ:at,qq=", "").replace("]", ""), args[2],
+                         int(args[3]) if len(args) >= 4 and args[3][0] != "{" else 1,
+                         json.loads(" ".join(args[4:]) if len(args) >= 5 else "{}"))
+            await su.finish("完成！")
+    except:
+        await _error.report()
