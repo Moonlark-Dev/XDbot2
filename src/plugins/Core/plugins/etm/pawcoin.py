822e1836032792d539a663c56048d8f756f516b2
This is XiaoDeng <1744793737@qq.com>
2023-07-07 12:15:14 +0800

    [update] 添加物品：PawCoin

---
+++ src/plugins/Core/plugins/etm/pawcoin.py
@@ -0,0 +1,10 @@
+from .item import Item
+
+
+class PawCoin(Item):
+    def on_register(self):
+        self.basic_data = {
+            "display_name": "猫掌币",
+            "display_message": "可用于兑换使用" # 具体的锅盖来写
+        }
+        self.item_id = "pawcoin"
