f906b294bb4dd32c0f89fe26b79c059682ae98ee
This is XiaoDeng <1744793737@qq.com>
2023-07-21 16:12:40 +0800

    [add] Create chatgptv2.py

--- src/plugins/Core/plugins/_utils.py
+++ src/plugins/Core/plugins/_utils.py
@@ -1,7 +1,21 @@
 import json
 import os.path
 
+# 快捷访问
+from nonebot import on_shell_command
+from nonebot import on_command
+from nonebot.rule import ArgumentParser
+from nonebot.permission import SUPERUSER
+from nonebot.params import CommandArg
+from nonebot.matcher import Matcher
+from . import _error as error
+from . import _lang as lang
+from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters.onebot.v11 import Message
+
 class Json:
+    
     def __init__(self, path: str) -> None:
         self.path = os.path.join("data", path)
         if not os.path.isfile(self.path):
@@ -11,11 +25,14 @@ class Json:
 
     def __setitem__(self, key: str, value: any) -> None:
         if value == None:
-            try:
-                self.data.pop(str(key))
-            except:
-                pass
+            self.pop(str(key))
         self.data[str(key)] = value
+    
+    def pop(self, key: str) -> any:
+        try:
+            return self.data.pop(key)
+        except:
+            return None
 
     def __getattr__(self, item: str) -> any:
         return self.get(item)
