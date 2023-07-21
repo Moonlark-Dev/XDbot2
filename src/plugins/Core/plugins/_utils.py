7567cc557ebdd8bcd466b3d02409905197127983
This is XiaoDeng <1744793737@qq.com>
2023-07-21 12:15:36 +0800

    [fix] fix a bug

--- src/plugins/Core/plugins/_utils.py
+++ src/plugins/Core/plugins/_utils.py
@@ -1,53 +1,35 @@
 import json
 import os.path
 
-class __JsonData__:
+class Json:
     def __init__(self, path: str) -> None:
-        self.path = path
+        self.path = os.path.join("data", path)
         if not os.path.isfile(self.path):
             self.data = {}
         else:
             self.data = json.load(open(self.path, encoding="utf-8"))
-    
-    def __setitem__(self, key: str, value: any) -> None:
-        self.data[str(key)] = value
-    
-    def get(self, key: str, default: any = None) -> None:
-        item = self.data.get(str(key))
-        if item is None:
-            self.data[key] = default
-            return self.get(key, default)
-        else:
-            return item
-
-    def __del__(self) -> None:
-        json.dump(self.data, open(self.path, "w", encoding="utf-8"))
-
-class Json:
-    def __init__(self, path: str) -> None:
-        self.path = os.path.join("data", path)
-
-    def __getitem__(self, key: str) -> any:
-        return self.get(key)
-
-    def get(self, key: str, default: any = None) -> None:
-        item = self.data.get(str(key))
-        if item is None:
-            self.data[key] = default
-            return self.get(key, default)
-        else:
-            return item
 
     def __setitem__(self, key: str, value: any) -> None:
         self.data[str(key)] = value
         self.save()
 
+    def __getattr__(self, item: str) -> any:
+        return self.get(item)
+    
+    def __getitem__(self, key: str) -> any:
+        return self.get(key)
+    
+    def __del__(self) -> None:
+        json.dump(self.data, open(self.path, "w", encoding="utf-8"))
+
+    def get(self, key: str, default: any = None) -> None:
+        item = self.data.get(key)
+        if item is None:
+            self.data[key] = default
+            return self.get(key, default)
+        else:
+            return item
+
     def items(self):
         return list(self.data.items())
 
-    def __getattr__(self, item: str) -> any:
-        match item:
-            case "data":
-                return __JsonData__(self.path)
-            case _:
-                return self.get(item)
