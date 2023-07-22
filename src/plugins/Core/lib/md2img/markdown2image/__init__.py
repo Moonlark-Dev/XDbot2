571ab20fc30bcea7e5461c53405cd73567874fda
This is XiaoDeng <1744793737@qq.com>
2023-07-22 15:04:28 +0800

    [update] 更新 MarkDown2Image版本

--- src/plugins/Core/lib/md2img/markdown2image/__init__.py
+++ src/plugins/Core/lib/md2img/markdown2image/__init__.py
@@ -1,13 +1,21 @@
 from . import parser
+import time
 from . import style
 # from rich import print
 
-
 def md2img(markdown: str, output_path: str) -> None:
-    ast = style.init_style(style.init_lists(
-        style.init_pre(style.init_links(parser.parse(markdown)))))
+    ast = style.init_style(
+        style.init_lists(
+            style.init_pre(
+                style.init_links(
+                    parser.parse(
+                        markdown
+                    )
+                )
+            )
+        )
+    )
     size = style.get_size(ast)
-    # print(ast)
     try:
         bg_color = style.default_style["html"]["background_color"]
     except KeyError:
