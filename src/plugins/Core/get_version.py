e320112bcf9f2717fbd253908c12e6209074462a
This is XiaoDeng <1744793737@qq.com>
2023-07-23 19:43:59 +0800

    [add] 全自动版本获取

---
+++ src/plugins/Core/get_version.py
@@ -0,0 +1,15 @@
+import os
+
+def get_version(is_develop):
+    # 判断开始提交
+    latest_tags = os.popen("git tag").read().splitlines()[-3:]
+    if latest_tags[1].split(".")[1] == latest_tags[2].split(".")[1]:
+        start_tag = latest_tags[2]
+        v1 = int(latest_tags[2].split(".")[1]) + 1
+    else:
+        start_tag = latest_tags[1]
+        v1 = int(latest_tags[2].split(".")[1])
+    # print(latest_tags, start_tag)
+    commit_count = int(os.popen(f'git rev-list --count {start_tag}..').read()) - 1
+    all_commit_count = int(os.popen(f'git rev-list --count HEAD').read())
+    return f"v2.{v1}.{commit_count}{'-dev' if is_develop else ''} (C0{all_commit_count})"
--- src/plugins/Core/plugins/version.py
+++ src/plugins/Core/plugins/version.py
@@ -1,15 +1,16 @@
 import traceback
 from nonebot import on_command
 import os
+from ._utils import Json, lang
 from . import _error as error
 from nonebot.matcher import Matcher
-
-git_log = os.popen("git log").read()
 
 
 @on_command("version", aliases={"版本信息"}).handle()
 async def show_version(matcher: Matcher):
     try:
-        await matcher.finish(git_log.split("\n\ncommit")[0])
+
+        git_log = os.popen(r"git log --pretty=format: %h\ %B").read().split("\n")[0]
+        await matcher.finish(lang.text("version.version", [Json("init.json")["version"], git_log]))
     except BaseException:
         await error.report(traceback.format_exc(), matcher)
