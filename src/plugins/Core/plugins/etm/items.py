e4e812299726225f51b1f0677e4de55b34c407c5
This is XiaoDeng <1744793737@qq.com>
2023-07-23 16:00:17 +0800

    [fix] 尝试修复循环引用的问题

--- src/plugins/Core/plugins/etm/items.py
+++ src/plugins/Core/plugins/etm/items.py
@@ -1,29 +1,4 @@
-from .dice import Dice
-from .auto_sign_coupon import AutoSignCoupon, AutoSignCouponActived
-from .talisman import Talisman
-from .book_and_quill import BookAndQuill
-from .pawcoin import PawCoin
-from .pouch import Pouch
-from .mystery_box import MysteryBoxLevel1
-from .vimcoin import VimCoin
-from .experience import Experience
-from .towel_zip import TowelZip, Towel
-
-ITEMS = {
-    "dice": Dice,
-    "mysterybox_lv1": MysteryBoxLevel1,
-    "book_and_quill": BookAndQuill,
-    "talisman": Talisman,
-    "pouch": Pouch,
-    "towel.zip": TowelZip,
-    "towel": Towel,
-    "vimcoin": VimCoin,
-    "pawcoin": PawCoin,
-    "exp": Experience,
-    "auto_sign_coupon": AutoSignCoupon,
-    "auto_sign_coupon_actived": AutoSignCouponActived
-}
-
+from .item_list import ITEMS
 
 def json2items(items, user_id=None):
     item_list = []
