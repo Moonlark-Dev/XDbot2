97407d93774cd86217e98d29f49f0b0ef4abcefe
This is XiaoDeng <1744793737@qq.com>
2023-07-27 16:29:52 +0800

    [add] 添加 su sudoers

--- src/plugins/Core/plugins/sudo.py
+++ src/plugins/Core/plugins/sudo.py
@@ -1,5 +1,6 @@
 from ._utils import *
 from nonebot.message import event_preprocessor
+from .su import su
 
 @event_preprocessor
 async def sudo_command(event: MessageEvent):
@@ -9,3 +10,18 @@ async def sudo_command(event: MessageEvent):
                 event.user_id = int(event.get_plaintext().strip().split(" ")[1])
                 event.message[0].data["text"] = " ".join(
                     event.message[0].data["text"].split(" ")[2:])
+
+@su.handle()
+async def su_sudo(message: Message = CommandArg()):
+    try:
+        argument = str(message).split(" ")
+        if argument[0] in ["sudoers"]:
+            sudoers: list = Json("sudo/sudoers.json").get("sudoers")
+            match argument[1]:
+                case "add":
+                    sudoers.append(int(argument[2]))
+                case "remove":
+                    sudoers.pop(sudoers.index(int(argument[2])))
+            Json("sudo/sudoers.json")["sudoers"] = list(set(sudoers))
+    except:
+        await error.report()
