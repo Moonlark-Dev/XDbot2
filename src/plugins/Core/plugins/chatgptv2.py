bf5e4ae9db641480c97e6466724cba67100d9127
This is XiaoDeng <1744793737@qq.com>
2023-07-29 14:45:48 +0800

    [update] update output

--- src/plugins/Core/plugins/chatgptv2.py
+++ src/plugins/Core/plugins/chatgptv2.py
@@ -137,6 +137,7 @@ async def handle_su_gpt(message: Message = CommandArg()) -> None:
                 if arguments[2] in ["add"]:
                     user_data = Json(f"gpt/users/{arguments[3]}.json")
                     user_data["token"] = user_data.get("token", 0) + int(arguments[4])
+                    await matcher.finish("完成！")
     except:
         await error.report()
             
