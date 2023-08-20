8cf689321f2be53fdc4670d567f5b9c7348dd36f
This is XiaoDeng <1744793737@qq.com>
2023-08-20 14:46:46 +0800

    [fix] fix a bug

--- src/plugins/Core/plugins/autoreply.py
+++ src/plugins/Core/plugins/autoreply.py
@@ -36,6 +36,6 @@ async def handle_reply(matcher: Matcher, event: GroupMessageEvent):
             )
 
             latest_use = time.time()
-            await matcher.finish(Message(":".join(reply.split(":")[1:])))
+            await matcher.finish(Message(reply))
     except:
         await error.report()
