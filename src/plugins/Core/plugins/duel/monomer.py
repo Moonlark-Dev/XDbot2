93a820a8e2255aa26c18b7c1f91e1f0283487a55
This is XiaoDeng <1744793737@qq.com>
2023-08-21 21:19:44 +0800

    [add] 添加部分函数

--- src/plugins/Core/plugins/duel/monomer.py
+++ src/plugins/Core/plugins/duel/monomer.py
@@ -20,9 +20,11 @@ class Monomer:
         self._default_data = base_properties.copy()
         self.gain_list = []
         self.triggers = {}
-        self.battle_skill_points: int = 3
+        self.battle_skill_points = 3
         self.buff = []
         self.hp = hp
+
+        self.enemy: Monomer
 
         self.get_weapons(weapons)
         self.get_ball(ball)
@@ -40,7 +42,7 @@ class Monomer:
         kits[self.ball["kit"]] = kits.get(self.ball["kit"], 0) + 1
         return kits
 
-    def get_kit_gain(self):
+    def get_kit_gain(self) -> None:
         for kit, count in list(self.get_kits().items()):
             if count >= 2:
                 self.gain_list += list(
@@ -73,7 +75,7 @@ class Monomer:
         if "gain" in self.weapons.keys():
             self.gain_list += self.weapons.items()
 
-    def get_ball(self, ball: str):
+    def get_ball(self, ball: str) -> None:
         self.ball = load_json(f"kits/{ball}.json")["ball"]
         self.ball["kit"] = ball
         if "gain" in self.ball.keys():
@@ -93,11 +95,12 @@ class Monomer:
                 percentage_gain[gain_name] = percentage_gain.get(gain_name, 0.0) + value
             else:
                 default_data[gain_name] = default_data.get(gain_name, 0) + value
+        _default_data = default_data.copy()
         for key, value in list(percentage_gain.items()):
-            default_data[key] = default_data.get(key, 0) * (1 + value)
+            default_data[key] = _default_data.get(key, 0) * (1 + value)
         return default_data
 
-    def effect_add_hp(self, effect: dict):
+    def effect_add_hp(self, effect: dict) -> None:
         if isinstance(effect["value"], float):
             self.hp += self.data["health"] * (
                 1 + effect["value"] + self.data["therapeutic_volume_bonus"]
@@ -110,7 +113,21 @@ class Monomer:
             self.triggers[effect["condition"]] = []
         self.triggers[effect["condition"]].append(effect["effect"])
 
-    def parse_effect(self, effect_block: list[dict]):
+    def effect_add_battle_skill_points(self, effect: dict) -> None:
+        if effect["target"]:
+            if self.enemy.battle_skill_points < 5:
+                self.enemy.battle_skill_points += 1
+        elif self.battle_skill_points < 5:
+            self.battle_skill_points += 1
+
+    def effect_remove_battle_skill_points(self, effect: dict) -> None:
+        if effect["target"]:
+            if self.enemy.battle_skill_points > 0:
+                self.enemy.battle_skill_points -= 1
+        elif self.battle_skill_points > 0:
+            self.battle_skill_points -= 1
+
+    def parse_effect(self, effect_block: list[dict]) -> None:
         for effect in effect_block:
             match effect["function"]:
                 case "add_hp":
@@ -120,3 +137,19 @@ class Monomer:
                 case "verify_probabilities":
                     if random.random() > effect["probability"]:
                         break
+                case "add_battle_skill_points":
+                    self.effect_add_battle_skill_points(effect)
+                case "remove_battle_skill_points":
+                    self.effect_remove_battle_skill_points(effect)
+                case "add_buff":
+                    self.effect_add_buff(effect)
+
+    def effect_add_buff(self, effect: dict) -> None:
+        pass
+
+    def prepare_before_action(self) -> None:
+        pass
+
+    def prepare_before_the_round(self) -> None:
+        pass
+
