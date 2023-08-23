493870a62b85072718d59c8cbaf64be0c0bdfc56
This is XiaoDeng <1744793737@qq.com>
2023-08-23 21:43:54 +0800

    [update] 完成合成模块

--- src/plugins/Core/plugins/etm/pawcoin.py
+++ src/plugins/Core/plugins/etm/pawcoin.py
@@ -3,7 +3,7 @@ from .item import Item
 
 class PawCoin(Item):
     def on_register(self):
-        self.basic_data = {
+        self.basic_data = { # type: ignore
             "display_name": "猫爪币",
             "display_message": "可用于兑换使用（没写完）",
             "useable": False
--- src/plugins/Core/plugins/synthesis.py
+++ src/plugins/Core/plugins/synthesis.py
@@ -53,6 +53,60 @@ def get_page(argv: list):
         return int(argv[1])
     except:
         return 1
+    
+SYNTHESIS_CONSUMES_PAWCOIN = 1
+
+def get_item(_id: int) -> dict | None:
+    for item in get_item_list():
+        if item["id"] == _id:
+            return item
+    return None
+
+def get_required_items(item_data: dict, count: int) -> dict:
+    required_items = {}
+    for item in item_data["required"]:
+        required_items[item["id"]] = required_items.get(item["id"], 0) + item["count"] * count
+    required_items["pawcoin"] = required_items.get("pawcoin", 0) + SYNTHESIS_CONSUMES_PAWCOIN * count
+    return required_items
+
+def check_required_items(required_items: dict[str, int], user_id: str):
+    for item in bag.get_user_bag(user_id):
+        if item.item_id in required_items.keys() and not item.data.get("locked", False):
+            required_items[item.item_id] -= item.count
+            if required_items[item.item_id] <= 0:
+                required_items.pop(item.item_id)
+    return len(list(required_items.keys())) == 0
+
+async def send_crafting_result(item_data: dict, count: int, user_id: str):
+    reduced_items = []
+    for item_id, c in list(get_required_items(item_data, count).items()):
+        reduced_items.append({
+            "id": item_id,
+            "count": c,
+            "data": {}
+        })
+    item_data["result"]["count"] = count
+    await finish("synthesis.done", [
+        items2text(reduced_items),
+        items2text(item_data["result"])
+    ], user_id)
+
+async def crafting_items(_id: int, count: int, user_id: str):
+    if (item_data := get_item(_id)) is None:
+        await finish("synthesis.item_not_found", [], user_id, False, True)
+    required_items = get_required_items(item_data, count) # type: ignore
+    if check_required_items(required_items, user_id):
+        user_bag = bag.get_user_bag(user_id)
+        for i in range(len(user_bag)):
+            if user_bag[i].item_id in required_items.keys():
+                user_bag[i].count -= (reduced_count := min(user_bag[i].count, required_items[user_bag[i].count]))
+                required_items[user_bag[i].count] -= reduced_count
+        bag.add_item(item_data["result"]["id"], count, item_data["result"]["data"])
+        await send_crafting_result(item_data, count, user_id)
+    else:
+        await finish("synthesis.synthesis.item_not_enough", [], user_id, False, True)
+
+
 
 @on_command("synthesis", aliases={"合成"}).handle()
 async def handle_synthesis_command(bot: Bot, event: MessageEvent, message: Message = CommandArg()):
