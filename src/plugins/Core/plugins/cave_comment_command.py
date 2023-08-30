d17986e738c8181ce141754f94e2bf999bfb8696
This is XiaoDeng <1744793737@qq.com>
2023-08-30 12:59:01 +0800

    [fix] fix a bug 屎山+1

--- src/plugins/Core/plugins/cave_comment_command.py
+++ src/plugins/Core/plugins/cave_comment_command.py
@@ -2,18 +2,20 @@ from ._utils import *
 from .cave import showEula
 
 # @on_command("cave-c", aliases={"cave-comment"}).handle()
+def check_cave_id(cave_id: str) -> bool:
+    data = json.load(open("data/cave.data.json", encoding="utf-8"))
+    return cave_id in data["data"]
 
 @create_command("cave-c", aliases={"cave-comment"})
 async def handle_cave_comment_command(bot: Bot, event: MessageEvent, message: Message):
     await showEula(event.get_user_id())
     if str(event.user_id) in json.load(open("data/cave.banned.json", encoding="utf-8")):
         await finish("cave.cannot_comment", [], str(event.user_id))
-    try:
-        cave_id = int(message.extract_plain_text().split(" ")[0])
-    except IndexError:
-        await finish("currency.wrong_argv", ["cave"], event.user_id, False, True)
-    except ValueError:
-        await finish("cave.notfound", [message.extract_plain_text().split(" ")[0]], event.user_id, False, True)
+    cave_id = message.extract_plain_text().split(" ")[0]
+    if not check_cave_id(cave_id):
+         await finish("cave.notfound", [], event.user_id, False, True)
+    if not " ".join(str(message).split(" ")[1:]).strip():
+         await finish("currency.wrong_argv", ["cave"], event.user_id, False, True)
     cave_comments = json.load(open("data/cave.comments.json", encoding="utf-8"))
     if cave_id not in cave_comments.keys():
             cave_comments[cave_id] = {"count": 1, "data": {}}
@@ -29,4 +31,4 @@ async def handle_cave_comment_command(bot: Bot, event: MessageEvent, message: Me
         f"{comment_text}\n"
         f"{event.get_session_id()}"
     ), feedback=False)
-    await finish("cave.commented", [cave_id, cave_comments[cave_id]['count'] - 1], event.user_id)
+    await finish("cave.commented", [cave_id, cave_comments[cave_id]['count'] - 1], event.user_id, True, True)
