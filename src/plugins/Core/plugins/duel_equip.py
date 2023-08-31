415e7b891f4573b4c93df8141e0928d0941e7270
This is XiaoDeng <1744793737@qq.com>
2023-08-31 13:51:59 +0800

    [fix] fix a bug

--- src/plugins/Core/plugins/duel_equip.py
+++ src/plugins/Core/plugins/duel_equip.py
@@ -1,13 +1,14 @@
+from typing import LiteralString
 from ._utils import *
 from .duel.monomer import load_json, Monomer
 
 
-def get_user_equip(user_id: int, _type: str = "weapons") -> str:
+def get_user_equip(user_id: int, _type: str = "weapons") -> dict:
     equip = Json(f"duel/u{user_id}.json").get(_type, "leather_case")
     return load_json(f"kits/{equip}.json")
 
 
-def get_weakness(user_id: int):
+def get_weakness(user_id: int) -> LiteralString:
     monomer = Monomer(
         Json(f"duel/u{user_id}.json").get("weapons", "leather_case"),
         Json(f"duel/u{user_id}.json").get("relics", {}),
@@ -21,18 +22,16 @@ def get_weakness(user_id: int):
 async def show_equip(event: MessageEvent, argv: list[str]) -> None:
     match get_list_item(argv, 1, ""):
         case "":
+            weapon_kit_data = get_user_equip(event.user_id, "weapons")
+            ball_kit_data = get_user_equip(event.user_id, "ball")
             await finish(
                 "duel_equip.show_euqip",
                 [
                     event.user_id,
-                    (weapon_kit_data := get_user_equip(event.user_id, "weapons"))[
-                        "weapons"
-                    ]["name"],
+                    weapon_kit_data["weapons"]["name"],
                     weapon_kit_data["level"],
-                    # 遗器
-                    (ball_kit_data := get_user_equip(event.user_id, "ball"))["ball"][
-                        "name"
-                    ],
+                    # 遗器（保留位置）
+                    ball_kit_data["ball"]["name"],
                     ball_kit_data["level"],
                     get_weakness(),
                 ],
