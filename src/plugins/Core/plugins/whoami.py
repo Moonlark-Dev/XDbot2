9a8762440c5e9ce968e45820ed5d768e5bbe0edb
XieXiLin <support@xiexilin.com>
2023-09-03 17:34:09 +0800

    [update] whoami: use create_command

--- src/plugins/Core/plugins/whoami.py
+++ src/plugins/Core/plugins/whoami.py
@@ -2,11 +2,9 @@ from _utils import *
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import MessageEvent, GroupMessageEvent, PrivateMessageEvent, MessageSegment
 
-whoami = on_command("whoami", aliases={"我是谁"})
-
-@whoami.handle()
+@create_command("whoami")
 async def _(event: MessageEvent):
-    await whoami.finish(MessageSegment.reply(event.message_id) + MessageSegment.text(f"{event.sender.nickname}"))
+    await finish(MessageSegment.reply(event.message_id) + MessageSegment.text(f"{event.sender.nickname}"))
 
 # [HELPSTART] Version: 2
 # Command: whoami
