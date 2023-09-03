3c1b9d660ea32eb7433b3929465963a7dc0c76da
XieXiLin <support@xiexilin.com>
2023-09-03 17:26:21 +0800

    [update] whoami: add help text

--- src/plugins/Core/plugins/whoami.py
+++ src/plugins/Core/plugins/whoami.py
@@ -7,3 +7,10 @@ whoami = on_command("whoami", aliases={"我是谁"})
 @whoami.handle()
 async def _(event: MessageEvent):
     await whoami.finish(MessageSegment.reply(event.message_id) + MessageSegment.text(f"{event.sender.nickname}"))
+
+# [HELPSTART] Version: 2
+# Command: whoami
+# Info: 我是谁？
+# Msg: 我是谁
+# Usage: whoami: 我是谁？
+# [HELPEND]
