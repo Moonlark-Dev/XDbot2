88a76a968b57b757ca542db15ec0b8d73044c55c
This is XiaoDeng <1744793737@qq.com>
2023-09-09 14:43:58 +0800

    [update] change HP

--- src/plugins/Core/plugins/etm/health.py
+++ src/plugins/Core/plugins/etm/health.py
@@ -2,8 +2,8 @@ from .._utils import *
 from ..duel import monomer
 
 
-def get_max_hp(user_id: int) -> float:
-    return monomer.Monomer(
+def get_max_hp(user_id: int) -> int:
+    return int(monomer.Monomer(
         (user_data := Json(f"duel/u{user_id}.json")).get("weapons", "leathcer_case"),
         user_data.get("realics", {}),
         user_data.get("ball", "leathcer_case"),
@@ -11,4 +11,4 @@ def get_max_hp(user_id: int) -> float:
         f"u{user_id}",
         user_data.get("weapons_level", 1),
         user_data.get("ball_level", 0)
-    ).data["health"]
+    ).data["health"])
