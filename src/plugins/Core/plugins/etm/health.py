fd85e441d08710738ed99a1671922404448dd38f
This is XiaoDeng <1744793737@qq.com>
2023-09-15 22:19:19 +0800

    [fix] fix a bug

--- src/plugins/Core/plugins/etm/health.py
+++ src/plugins/Core/plugins/etm/health.py
@@ -10,10 +10,10 @@ def get_data(user_id: int, key: str) -> any:
     return int(
         monomer.Monomer(
             (user_data := Json(f"duel/u{user_id}.json")).get(
-                "weapons", "leathcer_case"
+                "weapons", "leather_case"
             ),
             user_data.get("realics", {}),
-            user_data.get("ball", "leathcer_case"),
+            user_data.get("ball", "leather_case"),
             100,
             f"u{user_id}",
             user_data.get("weapons_level", 1),
