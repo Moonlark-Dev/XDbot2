c22b1ec63f40214705a88f9834c9390eb6f1c5dc
This is XiaoDeng <1744793737@qq.com>
2023-11-27 23:56:22 +0800

    [add] 原神，启动！

---
+++ src/plugins/Core/plugins/lgs.py
@@ -0,0 +1,12 @@
+from ._utils import *
+
+@create_command("launch-genshin-impact", {"lgs", "lgi"})
+async def _(_bot, event: MessageEvent, _message):
+    Json("data/lgs.count.json").add(event.get_user_id(), 1)
+    await finish("lgs.launch", [], event.user_id, False, True)
+
+# [HELPSTART] Version: 2
+# Command: lgs
+# Usage: launch-genshin-impact
+# Info: 打开原神
+# [HELPEND]
