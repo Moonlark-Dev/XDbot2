ceae9be7076b6d602abb32e95ada77cbae054f53
This is XiaoDeng <1744793737@qq.com>
2023-11-29 22:16:32 +0800

    [update] 使用 os.execv 代替写入文件进行重启

--- src/plugins/Core/plugins/su_restart.py
+++ src/plugins/Core/plugins/su_restart.py
@@ -3,9 +3,15 @@ from traceback import format_exc
 from .su import su
 from .etm import data
 from . import _error
-from nonebot.adapters.onebot.v11 import Message
-import time
+from nonebot.adapters.onebot.v11 import Messagee
 
+import sys
+import os
+
+def _restart() -> None:
+    script = sys.argv[0]
+    args = sys.argv[1:]
+    os.execv(sys.executable, [sys.executable] + [script] + args)
 
 @su.handle()
 async def restart(message: Message = CommandArg()):
@@ -14,8 +20,9 @@ async def restart(message: Message = CommandArg()):
         # 保存数据
         data.save_data()
         if argument[0] in ["restart", "重新启动"]:
-            with open("data/reboot.py", "w") as f:
-                f.write(str(time.time()))
-            await su.finish("重启命令已发出")
+            # with open("data/reboot.py", "w") as f:
+            #     f.write(str(time.time()))
+            await su.send("重启命令已发出")
+            _restart()
     except BaseException:
         await _error.report(format_exc(), su)
