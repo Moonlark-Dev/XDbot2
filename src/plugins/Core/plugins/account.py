5d589ee6a6c596732c175a7b3a0433373f75b948
This is XiaoDeng <1744793737@qq.com>
2023-12-24 14:12:26 +0800

    [fix] 修复本机路径导致无法发送图片的问题

--- src/plugins/Core/plugins/account.py
+++ src/plugins/Core/plugins/account.py
@@ -12,19 +12,21 @@ from .su import su
 
 ctrlGroup = json.load(open("data/ctrl.json", encoding="utf-8"))["control"]
 priority_accout = json.load(open("data/su.priority_accout.json", encoding="utf-8"))
-accouts = {}
-multiAccoutData = {}
+accounts = {}
+multiAccountData = {}
 account_manager = on_command("accout", aliases={"多帐号", "account"})
 driver = get_driver()
 
+def get_accounts_data() -> dict:
+    return multiAccountData
 
 @event_preprocessor
 async def multiAccoutManager(bot: Bot, event: GroupMessageEvent):
     try:
-        if event.group_id in multiAccoutData.keys():
+        if event.group_id in multiAccountData.keys():
             if (
-                str((await bot.get_login_info())["user_id"])
-                != multiAccoutData[event.group_id]
+                str(bot.self_id)
+                != multiAccountData[event.group_id]
             ):
                 raise IgnoredException("多帐号：忽略")
 
@@ -44,9 +46,9 @@ async def get_multiaccount_data(bot: Bot, message: Message = CommandArg()):
                 bots = get_bots()
                 reply += f"已连接账户：{len(bots.keys())}\n"
                 length = 1
-                for accout in list(bots.keys()):
+                for account in list(bots.keys()):
                     userData = await bot.get_stranger_info(
-                        user_id=(await bots[accout].get_login_info())["user_id"]
+                        user_id=(await bots[account].get_login_info())["user_id"]
                     )
                     reply += (
                         f"{length}. {userData['nickname']} ({userData['user_id']})\n"
@@ -56,8 +58,8 @@ async def get_multiaccount_data(bot: Bot, message: Message = CommandArg()):
                 # 分配情况
                 length = 1
                 reply = "「XDbot2 Multi-Account 群聊分配」\n"
-                for group in list(multiAccoutData.keys()):
-                    reply += f"{length}. {group}: {multiAccoutData[group]}\n"
+                for group in list(multiAccountData.keys()):
+                    reply += f"{length}. {group}: {multiAccountData[group]}\n"
                     length += 1
                 await su.finish(reply)
     except BaseException:
@@ -67,24 +69,24 @@ async def get_multiaccount_data(bot: Bot, message: Message = CommandArg()):
 @driver.on_bot_connect
 @driver.on_bot_disconnect
 async def reloadMuiltData():
-    global multiAccoutData, bots, accouts
+    global multiAccountData, bots, accounts
     bots = get_bots()
-    accouts = {}
-    multiAccoutData = {}
+    accounts = {}
+    multiAccountData = {}
     for key in list(bots.keys()):
         bot = bots[key]
         groups = await bot.get_group_list()
         for group in groups:
-            if group["group_id"] not in accouts.keys():
-                accouts[group["group_id"]] = [key]
+            if group["group_id"] not in accounts.keys():
+                accounts[group["group_id"]] = [key]
             else:
-                accouts[group["group_id"]].append(key)
-            if group["group_id"] not in multiAccoutData.keys():
-                multiAccoutData[group["group_id"]] = key
+                accounts[group["group_id"]].append(key)
+            if group["group_id"] not in multiAccountData.keys():
+                multiAccountData[group["group_id"]] = key
             elif key in priority_accout["accouts"]:
-                multiAccoutData[group["group_id"]] = key
+                multiAccountData[group["group_id"]] = key
     json.dump(
-        multiAccoutData, open("data/su.multiaccoutdata.ro.json", "w", encoding="utf-8")
+        multiAccountData, open("data/su.multiaccoutdata.ro.json", "w", encoding="utf-8")
     )
 
 
@@ -92,15 +94,15 @@ async def reloadMuiltData():
 async def mulitaccout_manager(
     event: GroupMessageEvent, message: Message = CommandArg()
 ):
-    global multiAccoutData
+    global multiAccountData
     try:
         argument = str(message).split(" ")
         qq = event.get_user_id()
         if argument[0] == "set":
-            if argument[1] in accouts[event.group_id]:
-                multiAccoutData[event.group_id] = argument[1]
+            if argument[1] in accounts[event.group_id]:
+                multiAccountData[event.group_id] = argument[1]
                 json.dump(
-                    multiAccoutData,
+                    multiAccountData,
                     open("data/su.multiaccoutdata.ro.json", "w", encoding="utf-8"),
                 )
                 await account_manager.finish(
@@ -110,7 +112,7 @@ async def mulitaccout_manager(
                 await account_manager.finish(_lang.text("su.accout_not_found"), user=qq)
         elif argument[0] == "list":
             await account_manager.finish(
-                _lang.text("su.accout_list", [accouts[event.group_id]], qq)
+                _lang.text("su.accout_list", [accounts[event.group_id]], qq)
             )
         elif argument[0] == "reload":
             await reloadMuiltData()
--- src/plugins/Core/plugins/bots.py
+++ src/plugins/Core/plugins/bots.py
@@ -1,32 +1,82 @@
-from typing import Any
-from nonebot.adapters.onebot.v11 import Bot
-from nonebot import get_bots
+import base64
+from typing import Optional, cast
 from nonebot import get_driver
-from nonebot.exception import ActionFailed, MockApiException
+from ._utils import *
+from nonebot.adapters import Bot as BaseBot
+from nonebot.exception import MockApiException
+from .account import get_accounts_data
 
 
-async def on_called_api(
-    bot: Bot,
-    exception: Exception | None,
-    api: str,
-    data: dict[str, Any],
-    result: dict[str, Any],
-):
-    if not exception:
+class GroupNotFoundException(Exception): pass
+
+def select_bot_by_group_id(group_id: int) -> Optional[Bot]:
+    """
+    使用群号获取 Bot 对象
+
+    Args:
+        group_id (int): 群号
+
+    Returns:
+        Optional[Bot]: Bot 对象, 查找失败则返回 None
+    """
+    return get_accounts_data().get(group_id)
+
+
+async def check_group_id(bot: Bot, api: str, data: dict) -> None:
+    """
+    检查并修复 API 调用中的群号
+
+    Args:
+        bot (Bot): 对应 Bot
+        api (str): API 终结点
+        data (dict): 调用参数
+
+    Raises:
+        MockApiException: 阻止 API 调用
+    """
+    if "group_id" not in data.keys():
         return
-    # print(locals())
-    if not isinstance(exception, ActionFailed):
-        return
-    if not data.get("__retry__", True):
-        return
-    for b in get_bots().values():
-        try:
-            data["__retry__"] = False
-            raise MockApiException(await b.call_api(api, **data))
-        except ActionFailed as e:
+    groups = await bot.get_group_list()
+    for group in groups:
+        if group["group_id"] == int(data["group_id"]):
+            return
+    group_id = int(data["group_id"])
+    if (correct_bot := select_bot_by_group_id(group_id)):
+        raise MockApiException(await correct_bot.call_api(api, **data))
+    raise GroupNotFoundException(f"区域外的群号: {group_id}")
+
+def check_message_images(original_message: Message) -> Message:
+    message = original_message.copy()
+    length = -1
+    for segment in message:
+        length += 1
+        if segment.type != "image":
             continue
+        file = cast(str, segment.data.get("file", ""))
+        if not file.startswith("file://"):
+            continue
+        with open(file[7:], "rb") as f:
+            data = base64.b64encode(f.read()).decode('utf-8')
+        message[length].data["file"] = f"base64://{data}"
+    return message
+
+async def on_calling_api(bot: BaseBot, api:  str, data: dict) -> None:
+    if not isinstance(bot, Bot):
+        return
+    await check_group_id(bot, api, data)
+    if "message" in data.keys() and isinstance(data["message"], Message):
+        data["message"] = check_message_images(data["message"])
+    
+
+
 
 
 @get_driver().on_bot_connect
-async def _(bot: Bot):
-    bot.on_called_api(on_called_api)
+async def _(bot: Bot) -> None:
+    """
+    注册调用接口前钩子函数
+
+    Args:
+        bot (Bot): Bot
+    """
+    bot.on_calling_api(on_calling_api)
