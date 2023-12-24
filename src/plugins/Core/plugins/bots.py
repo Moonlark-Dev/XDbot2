e110abd44927cfc2e4fb6ef1c1fbd2469e8f4f6a
This is XiaoDeng <1744793737@qq.com>
2023-12-24 16:56:00 +0800

    [add] 添加合并转发拉取提示
    Add prompt for pulling merged forwards
    
    - Add prompt for pulling information from a merged forward message
      in order to provide context and enhance user experience when using
      certain forwarded message features. This feature improves user
      understanding when handling forwarded messages.

--- src/plugins/Core/plugins/bots.py
+++ src/plugins/Core/plugins/bots.py
@@ -63,6 +63,43 @@ def check_message_images(original_message: Message) -> Message:
         message[length].data["file"] = f"base64://{data}"
     return message
 
+async def check_forward_node(bot: Bot, api: str, _data: dict) -> None:
+    if api not in ["send_group_forward_msg", "send_private_forward_msg"] or "__info_id__" in _data.keys():
+        return
+    data = _data.copy()
+    user_id = data.get("user_id", "default")
+    for i in range(len(data["messages"])):
+        node = data["messages"][i]
+        if isinstance(node, MessageSegment):
+            message = data["messages"][i].data.copy()
+        else:
+            message = data["messages"][i]["data"].copy()
+        if not isinstance(message["content"], Message):
+            message["content"] = Message(message["content"])
+        if (user := int(message.get("uin") or message.get("user_id"))) != int(bot.self_id):
+            message["content"].insert(0, MessageSegment.text(
+                lang.text(
+                    "bots.message_from",
+                    [message["nickname"] or (await bot.get_stranger_info(user_id=user))["nickname"]],
+                    user_id
+                )
+            ))
+        message["content"] = check_message_images(message["content"])
+        if isinstance(node, MessageSegment):
+            data["messages"][i].data = message
+        else:
+            data["messages"][i]["data"] = message
+    data["__info_id__"] = (await bot.send_msg(
+        message=lang.text("bots.waiting_node", [], user_id),
+        message_type=api.replace("send_", "").replace("_forward_msg", ""),
+        group_id=data.get("group_id"),
+        user_id=int(user_id) if user_id != "default" else None
+    ))["message_id"]
+    raise MockApiException(
+        await bot.call_api(api, **data)
+    )
+
+
 
 async def on_calling_api(bot: BaseBot, api: str, data: dict) -> None:
     if not isinstance(bot, Bot):
@@ -73,7 +110,7 @@ async def on_calling_api(bot: BaseBot, api: str, data: dict) -> None:
     for key, value in data.items():
         if key.endswith("_id") and isinstance(value, str):
             data[key] = int(value)
-
+    await check_forward_node(bot, api, data)
 
 async def on_called_api(
     bot: BaseBot,
@@ -84,22 +121,26 @@ async def on_called_api(
 ):
     if not isinstance(bot, Bot):
         return
-    if not (isinstance(exception, ActionFailed) and api == "get_stranger_info"):
-        return
-    if "__xdbot_retry__" in data.keys():
-        return
-    for b in get_bots().values():
-        try:
-            raise MockApiException(
-                await b.get_stranger_info(**data, __xdbot_retry__=True)
-            )
-        except ActionFailed:
-            continue
-        except NetworkError:
-            continue
-    raise MockApiException(
-        {"user_id": data["user_id"], "nickname": f'<User:{data["user_id"]}'}
-    )
+    if isinstance(exception, ActionFailed) and api == "get_stranger_info":
+        if "__xdbot_retry__" in data.keys():
+            return
+        for b in get_bots().values():
+            try:
+                raise MockApiException(
+                    await b.get_stranger_info(**data, __xdbot_retry__=True)
+                )
+            except ActionFailed:
+                continue
+            except NetworkError:
+                continue
+        raise MockApiException(
+            {"user_id": data["user_id"], "nickname": f'<U:{data["user_id"]}>'}
+        )
+    elif api in ["send_group_forward_msg", "send_private_forward_msg"] and "__info_id__" in data.keys():
+        await bot.delete_msg(message_id=data["__info_id__"])
+        
+
+    
 
 
 @get_driver().on_bot_connect
