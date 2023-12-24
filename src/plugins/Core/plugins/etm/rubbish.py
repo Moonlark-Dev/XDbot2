cf3fecc9d3b56361b3ffabb44a892fff51aa2bd9
This is XiaoDeng <1744793737@qq.com>
2023-12-24 13:24:14 +0800

    [fix] fix a bug Fix bug causing incorrect item display names to be used in the 'use_item' message. Amend 'use_item' text parameter to correctly display item names from item data.

--- src/plugins/Core/plugins/etm/rubbish.py
+++ src/plugins/Core/plugins/etm/rubbish.py
@@ -47,6 +47,6 @@ class CommonRubbish(Item):
         length = 1
         for item in items:
             add_item(self.user_id, item.item_id, item.count, item.data)
-            message += self.text("use_item", [length, item.item_id, item.count])
+            message += self.text("use_item", [length, item.data["display_name"], item.count])
             length += 1
         return [message]
