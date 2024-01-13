7191cc94f4b7615b4bdcd7d10697a1e53bc26800
This is XiaoDeng <1744793737@qq.com>
2024-01-13 10:03:14 +0800

    [update] 不再转发私聊消息

--- src/plugins/Core/plugins/forward.py
+++ src/plugins/Core/plugins/forward.py
@@ -16,18 +16,18 @@ groupForward = on_message()
 forwardData = json.load(open("data/forward.groupList.json", encoding="utf-8"))
 
 
-@privateForward.handle()
-async def privateForwardHandle(bot: Bot, event: PrivateMessageEvent):
-    try:
-        if event.get_message().extract_plain_text()[1:11] != "gpt-config":
-            await _error.report(
-                f"""{_lang.text("forward.private")}
-{_lang.text("forward._user")}{(await bot.get_stranger_info(user_id=event.get_user_id()))['nickname']} ({event.get_user_id()})
-{event.get_message()}"""
-            )
+# @privateForward.handle()
+# async def privateForwardHandle(bot: Bot, event: PrivateMessageEvent):
+#     try:
+#         if event.get_message().extract_plain_text()[1:11] != "gpt-config":
+#             await _error.report(
+#                 f"""{_lang.text("forward.private")}
+# {_lang.text("forward._user")}{(await bot.get_stranger_info(user_id=event.get_user_id()))['nickname']} ({event.get_user_id()})
+# {event.get_message()}"""
+#             )
 
-    except Exception:
-        await _error.report(traceback.format_exc(), privateForward)
+#     except Exception:
+#         await _error.report(traceback.format_exc(), privateForward)
 
 
 @groupForward.handle()
