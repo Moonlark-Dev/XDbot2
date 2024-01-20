a82f278efaa761b126e54bcefa0f7a8310ecdfd7
This is XiaoDeng <1744793737@qq.com>
2024-01-20 19:59:16 +0800

    [fix] fix a bug

--- src/plugins/Core/plugins/latex.py
+++ src/plugins/Core/plugins/latex.py
@@ -2,7 +2,7 @@ from ._utils import *
 from io import BytesIO
 import matplotlib.pyplot as plt
 
-plt.rcParams['text.usetex'] = True
+# plt.rcParams['text.usetex'] = True
 
 
 def generate_image(latex_formula: str) -> MessageSegment:
