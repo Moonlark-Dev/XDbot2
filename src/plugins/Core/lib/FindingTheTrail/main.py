63d0d4d8c411ba5f90a7525c2bd4e9132d637514
This is XiaoDeng <1744793737@qq.com>
2024-02-17 21:54:53 +0800

    [update] 优化搜索器性能

--- src/plugins/Core/lib/FindingTheTrail/main.py
+++ src/plugins/Core/lib/FindingTheTrail/main.py
@@ -4,9 +4,21 @@ from .argv import ARGUMENTS
 
 difficulty = "normal"
 
-game_map = generate(**ARGUMENTS[difficulty]["map"])
-print("Map:")
-print("\n".join(str(row) for row in game_map))
+# game_map = generate(**ARGUMENTS[difficulty]["map"])
+# print("Map:")
+# print("\n".join(str(row) for row in game_map))
+
+game_map = [
+    [1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1],
+    [1, 5, 2, 0, 7, 0, 0, 0, 2, 0, 0, 1],
+    [1, 0, 7, 0, 0, 0, 0, 5, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 7, 5, 0, 0, 1],
+    [1, 7, 2, 0, 0, 2, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 1],
+    [1, 0, 1, 0, 0, 2, 0, 5, 0, 1, 0, 1],
+    [1, 0, 4, 2, 0, 0, 7, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
 # game_map = [
 #     [1, 1, 1, 3, 1, 1, 1, 1],
