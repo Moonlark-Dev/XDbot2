2cf6b2ba36aa44b4a6d32c9341c725d21bc004ec
XiaoDeng3386 <xiaodeng3386@itcdt.top>
2024-03-26 20:41:46 +0800

    [add] help

--- src/plugins/Core/plugins/extra.py
+++ src/plugins/Core/plugins/extra.py
@@ -7,4 +7,10 @@
 # Msg: 查看网络状态
 # Info: 查看 XDbot2 有关服务状态
 # Usage: 服务状态
-# [HELPEND]
+# Command: motd
+# Msg: MC服务器信息
+# Info: 查询 Minecraft 服务器信息
+# Usage: motd：查看插件帮助
+# Usage: motdpe <server>：查询基岩版服务器信息
+# Usage: motd <server>：查询 Java 版服务器信息
+#[HELPEND]
